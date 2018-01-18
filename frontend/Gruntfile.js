d141efbaa441e1acad45f067ab8994c9a3c65821
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-18 00:56:00 +0000

    now watching

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,57 +1,66 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    browserify: {
-      dist: {
-        src: ['app/index.js'],
-        dest: 'dist/bundle.js',
-        options: {
-          transform: ['babelify']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            dist: {
+                src: ['app/index.js'],
+                dest: 'dist/bundle.js',
+                options: {
+                    transform: ['babelify']
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/bundle.js': ['<%= browserify.dist.dest %>']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'dist/styles.css': ['css/*.css']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                        expand: true,
+                        cwd: 'html',
+                        src: ['**'],
+                        dest: 'dist/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'assets',
+                        src: ['**'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        watch: {
+            app: {
+                files: ['app/**/!(*.test).js'],
+                tasks: ['browserify', 'uglify']
+            },
+            assets: {
+                files: ['html/**/*', 'assets/**/*'],
+                tasks: ['copy']
+            },
+            css: {
+                files: ['css/**/*'],
+                tasks: ['cssmin']
+            }
         }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'dist/bundle.js': ['<%= browserify.dist.dest %>']
-        }
-      }
-    },
-    cssmin: {
-      target: {
-        files: {
-          'dist/styles.css': ['css/*.css']
-        }
-      }
-    },
-    copy: {
-      main: {
-        files: [
-          {expand: true, cwd: 'html', src: ['**'], dest: 'dist/'},
-          {expand: true, cwd: 'assets', src: ['**'], dest: 'dist/'}
-        ]
-      }
-    },
-    watch: {
-      app: {
-        files: ['app/**/!(*.test).js'],
-        tasks: ['browserify', 'uglify']
-      },
-      assets: {
-        files: ['html/**/*', 'assets/**/*'],
-        tasks: ['copy']
-      },
-      css: {
-        files: ['css/**/*'],
-        tasks: ['cssmin']
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    });
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('static', ['browserify', 'uglify', 'cssmin', 'copy']);
+    grunt.registerTask('static', ['browserify', 'uglify', 'cssmin', 'copy']);
 };
