de5afe5cc5bfc8ac8a6b24d663b14457039b3c14
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-19 16:07:14 +0000

    text to example

--- backend/backend/example.py
+++ backend/backend/example.py
@@ -6,8 +6,8 @@ def example():
     >>> example()
     Content-Type: text/html
     <BLANKLINE>
-    <!DOCTYPE html><html lang="en"><head><title>Example</title></head><body></body></html>
+    <!DOCTYPE html><html lang="en"><head><title>Example</title></head><body>this is an example</body></html>
     """
     print('Content-Type: text/html')
     print()
-    print('<!DOCTYPE html><html lang="en"><head><title>Example</title></head><body></body></html>')
+    print('<!DOCTYPE html><html lang="en"><head><title>Example</title></head><body>this is an example</body></html>')
