625b0c74d32da3625c3e0db335cdda0b18e5be20
Arash Hedayatpour <aulee7@outlook.ie>
2018-01-19 23:27:04 +0000

    Implement initial revision of sendJSONToServer()

--- frontend/app/sendJSON.js
+++ frontend/app/sendJSON.js
@@ -1,5 +1,22 @@
+// Generate stringified JSON object from JavaScript object
 export function generateJSON() {
-    var data = {"type":"gameStart"};
-    var stringifiedData = JSON.stringify(data);
+    const data = { type: 'gameStart' };
+    const stringifiedData = JSON.stringify(data);
     return stringifiedData;
+}
+
+// Send stringified JSON data to a server through an AJAX request
+export function sendJSONToServer() {
+    const stringifiedData = generateJSON();
+
+    const ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.onreadystatechange = function receiveResponse() {
+        if (this.readyState === 4 && this.status === 200) {
+            // Take action based on server response
+        }
+    };
+    ajaxRequest.open('POST', '../../backend/backend/example.py', true);
+    ajaxRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    ajaxRequest.send(`x=|${stringifiedData}`);
+    return false;
 }
