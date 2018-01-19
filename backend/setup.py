f8996a80c8138f36df9ec380d8e18ddba30c97bb
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-19 18:27:11 +0000

    bugfix

--- backend/setup.py
+++ backend/setup.py
@@ -23,6 +23,6 @@ setup(
     ],
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['%s=%s' % i for i in pages],
+        'console_scripts': list(map('='.join, pages.items())),
     },
 )
