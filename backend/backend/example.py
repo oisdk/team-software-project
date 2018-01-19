7dd6f650ede575dbcd2809270f56985a0cb8095f
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-19 16:28:14 +0000

    Revert "Merge pull request #10 from oisdk/importing-library-example"
    This reverts commit df074cf5544834a7abeba8972029e49707f0f955, reversing
    changes made to 99e8c13efee0370cd9ac85d12ca411207765af25.

--- backend/backend/example.py
+++ backend/backend/example.py
@@ -1,7 +1,5 @@
 """an example"""
 
-import dateutil.parser
-
 def example():
     """serves an example web page
 
@@ -11,15 +9,13 @@ def example():
     <!DOCTYPE html>
     <html lang="en">
     <head><title>Example</title></head>
-    <body>this is an example: 2003-10-11</body>
+    <body>this is an example</body>
     </html>
     """
-    now = dateutil.parser.parse("Sat Oct 11 17:13:46 UTC 2003")
-    today = now.date()
     print('Content-Type: text/html')
     print()
     print("""<!DOCTYPE html>
 <html lang="en">
 <head><title>Example</title></head>
-<body>this is an example: %s</body>
-</html>""" % today)
+<body>this is an example</body>
+</html>""")
