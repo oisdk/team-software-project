b0adb1c53421f399f85eaf8a3dcbf4eeeda568eb
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-19 16:20:34 +0000

    used dateutil library

--- backend/backend/example.py
+++ backend/backend/example.py
@@ -11,13 +11,15 @@ def example():
     <!DOCTYPE html>
     <html lang="en">
     <head><title>Example</title></head>
-    <body>this is an example</body>
+    <body>this is an example: 2003-10-11</body>
     </html>
     """
+    now = parse("Sat Oct 11 17:13:46 UTC 2003")
+    today = now.date()
     print('Content-Type: text/html')
     print()
     print("""<!DOCTYPE html>
 <html lang="en">
 <head><title>Example</title></head>
-<body>this is an example</body>
-</html>""")
+<body>this is an example: %s</body>
+</html>""" % today)
