92494fd4eabbb1b46a322145e488b52155ef26de
Arash Hedayatpour <aulee7@outlook.ie>
2018-01-20 15:50:24 +0000

    Implement initial revision of process_client_json test file

---
+++ backend/tests/test_process_client_json.py
@@ -0,0 +1,30 @@
+import unittest
+import doctest
+import backend.process_client_json
+
+sample_dictionary_object = {"key":"value"}
+
+class TestProcessClientJSON(unittest.TestCase):
+
+    def test_roll_dice(self):
+        for i in range(100):
+            dice_roll = backend.process_client_json.roll_dice()
+            self.assertTrue(dice_roll[0] + dice_roll[1] >= 2 and
+                            dice_roll[0] + dice_roll[1] <= 12)
+
+    def test_interpret_request(self):
+        self.assertEqual(backend.process_client_json.interpret_message(),
+                         sample_dictionary_object)
+
+    def test_manager(self):
+        self.assertEqual(backend.process_client_json.manager_function(), "ack")
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(backend.process_client_json))
+    return tests
+
+
+if __name__ == '__main__':
+
+    unittest.main()
