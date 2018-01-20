9d1c1c57d44b6034dd16376f980ffdbfe5b57c28
Arash Hedayatpour <aulee7@outlook.ie>
2018-01-20 15:19:28 +0000

    Implement initial revision of process_client_json

--- backend/backend/process_client_json.py
+++ backend/backend/process_client_json.py
@@ -1,1 +1,32 @@
+"""Module providing functionality to retrieve requests, parse JSON, and call on
+   roll die functionality.
 
+"""
+from cgitb import enable
+from cgi import FieldStorage
+from backend.roll_die import roll_two_dice
+import json
+
+enable()
+
+def manager_function():
+    print('Content-Type: text/html')
+    print()
+    client_request = interpret_request()
+    dice_result = roll_dice()
+    print("ack")
+    return (client_request, dice_result)
+
+def interpret_request():
+    print('Content-Type: text/html')
+    print()
+
+    form_data = FieldStorage()
+
+    json_data = form_data.getFirst("jsonrequest")
+    client_request = json.loads(json_data)
+
+    return client_request
+
+def roll_dice():
+    return roll_two_dice()
