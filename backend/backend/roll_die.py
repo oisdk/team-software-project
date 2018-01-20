4a31d7db36c434ed76e03a3522fc10de319149c7
Arash Hedayatpour <aulee7@outlook.ie>
2018-01-20 14:01:56 +0000

    Combine roll_two_dice functionality into roll_die module

--- backend/backend/roll_die.py
+++ backend/backend/roll_die.py
@@ -8,3 +8,14 @@ def roll_dice():
        to represent the numbers of a die.
     """
     return randint(1, 6)
+
+
+def roll_two_dice():
+    """Simulates the rolling of two 6-sided dice.
+
+    Returns:
+        An int tuple representing the result of the two dice rolls.
+
+    """
+    dice_result = (roll_dice(), roll_dice())
+    return dice_result
