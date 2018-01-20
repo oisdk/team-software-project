4d38ac329f7f4e1fd2da932c7589bf0337d55b9e
Arash Hedayatpour <aulee7@outlook.ie>
2018-01-20 11:41:53 +0000

    Comment roll_two_dice function

--- backend/backend/roll_two_dice.py
+++ backend/backend/roll_two_dice.py
@@ -1,8 +1,14 @@
 from roll_die import roll_dice
 
 def roll_two_dice():
+    """ Simulates the rolling of two dice.
+
+        Returns:
+            An int array representing the result of the two dice rolls.
+    """
+
     dice_result = []
     dice_result += [roll_dice()]
     dice_result += [roll_dice()]
-    
+
     return dice_result
