35f54b2f1f3a07d5cc899ea7ab0bb6e34729beda
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-24 14:31:08 +0000

    Correct errors in python

--- backend/backend/polling.py
+++ backend/backend/polling.py
@@ -1,6 +1,7 @@
 """Enables polling the server for updates."""
 
 import json
+import sys
 import cgitb
 
 cgitb.enable()
@@ -8,4 +9,4 @@ cgitb.enable()
 def get_state_update(input=sys.stdin, output=sys.stdout):
     """Respond to client requests with the current state."""
     output.write('Content-Type: application/json\n\n')
-    json.dump({"your_turn": True})
+    json.dump({"your_turn": True}, output)
