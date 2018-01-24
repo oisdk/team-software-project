4c1b81615a30e579db81365163095d2bbd8e76cc
gabystephanov <115342151@umail.ucc.ie>
2018-01-24 23:38:04 +0000

    Checks if checkbox has been checked
    If a username is entered and checkbox is checked, username text box is disabled and roll_die button is enabled.

---
+++ frontend/app/checkButton.js
@@ -0,0 +1,43 @@
+(function() {
+    
+    var username;
+    var confirm_username;
+    var checker;
+    
+    document.addEventListener('DOMContentLoaded', init, false);
+
+    function init() {
+        username = document.querySelector('#username');
+	    confirm_username = document.querySelector('#confirm_username');
+	    checker = document.querySelector('#checker');
+	    confirm_username.addEventListener('click', box_checked, false);
+    }
+
+    function box_checked(){
+        if(confirm_username.checked){
+            if(!checkTextField()){
+    	        username.disabled = true;
+    	        confirm_username.disabled = true;
+    	        document.getElementById("roll_die").disabled = false;
+    	        checker.innerHTML = 'Username is confirmed';
+    	    }else{
+    	        checker.innerHTML = 'Username hasn\'t been entered';
+    	    }
+        }
+    }
+    
+    function checkTextField(){
+        var username_value = username.value;
+    	if ( username_value !== null && username_value.trim() !== ''){
+    	    return false;
+        }
+        else if (username_value === null || username_value.trim() === ''){
+            return true;
+        }
+        else{
+	        return true;
+	    }
+    }
+    
+})();
+
