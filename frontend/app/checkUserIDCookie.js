129ba75fd21312ecdbc176dedf354b98562763ba
arhh <aulee7@icloud.com>
2018-01-26 15:31:39 +0000

    Write initial incomplete checkUserCookie implementation

--- frontend/app/checkUserIDCookie.js
+++ frontend/app/checkUserIDCookie.js
@@ -1,1 +1,11 @@
-// 
+export function checkUserCookie() {
+    let browserCookies = document.cookie;
+    if (browserCookies !== "") {
+        browserCookies = browserCookies.split("; ");
+        let username = getUsernameCookie();
+        let user_id = getUserIDCookie();
+        if (username !== null && user_id !== null) {
+            // Create ajax request for either create username / join-create game
+        }
+    }
+}
