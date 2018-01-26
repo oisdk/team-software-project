f807e025496bf4a438764c9fb0012d0a82ad4e33
arhh <aulee7@icloud.com>
2018-01-26 23:00:45 +0000

    Comment out checkUserIDCookie in index.js until code functions properly

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,9 +1,8 @@
 import * as sendJSON from './sendJSON';
-import * as checkUserIDCookie from './checkUserIDCookie';
 
 
 window.onload = () => {
-    const existingUser = checkUserIDCookie.checkUserDetails();
+    // const isExistingUser = checkUserIDCookie.checkUserDetails();
     document.getElementById('roll_die').onclick = () => {
         sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
             if (req.readyState === 4 && req.status === 200) {
