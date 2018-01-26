e71612df941d7eea5bbd4dff30d1775021e9f8b0
arhh <aulee7@icloud.com>
2018-01-26 22:52:53 +0000

    Add functionality to check if visitor has userid cookie Check is performed immediately upon arriving at the initial HTML page.

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,7 +1,9 @@
 import * as sendJSON from './sendJSON';
+import * as checkUserIDCookie from './checkUserIDCookie';
 
 
 window.onload = () => {
+    const existingUser = checkUserIDCookie.checkUserDetails();
     document.getElementById('roll_die').onclick = () => {
         sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
             if (req.readyState === 4 && req.status === 200) {
