e5f23276b3fe1e0d46c28f5906f75c7b4661b270
arhh <aulee7@icloud.com>
2018-01-27 11:11:09 +0000

    Rename constant containing call to checkUserIDCookie for clarity. The new isPlayerInitialised variable contains a boolean: true if the user visited before and their player object was instantiated, and false if the visitor is new (and must be redirected to the create user page).

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -2,7 +2,7 @@ import * as sendJSON from './sendJSON';
 
 
 window.onload = () => {
-    // const isExistingUser = checkUserIDCookie.checkUserDetails();
+    // const isPlayerInitialised = checkUserIDCookie.checkUserDetails();
     document.getElementById('roll_die').onclick = () => {
         sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
             if (req.readyState === 4 && req.status === 200) {
