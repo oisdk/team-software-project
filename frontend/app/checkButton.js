452713090e9a535d619936f506ea05c0ec888b49
gabystephanov <115342151@umail.ucc.ie>
2018-01-27 23:01:41 +0000

    Tabified the file

--- frontend/app/checkButton.js
+++ frontend/app/checkButton.js
@@ -1,43 +1,45 @@
 (function() {
-    
-    var username;
-    var confirm_username;
-    var checker;
-    
-    document.addEventListener('DOMContentLoaded', init, false);
 
-    function init() {
-        username = document.querySelector('#username');
-	    confirm_username = document.querySelector('#confirm_username');
-	    checker = document.querySelector('#checker');
-	    confirm_username.addEventListener('click', box_checked, false);
-    }
+	var username;
+	var confirm_username;
+	var checker;
 
-    function box_checked(){
-        if(confirm_username.checked){
-            if(!checkTextField()){
-    	        username.disabled = true;
-    	        confirm_username.disabled = true;
-    	        document.getElementById("roll_die").disabled = false;
-    	        checker.innerHTML = 'Username is confirmed';
-    	    }else{
-    	        checker.innerHTML = 'Username hasn\'t been entered';
-    	    }
-        }
-    }
-    
-    function checkTextField(){
-        var username_value = username.value;
-    	if ( username_value !== null && username_value.trim() !== ''){
-    	    return false;
-        }
-        else if (username_value === null || username_value.trim() === ''){
-            return true;
-        }
-        else{
-	        return true;
-	    }
-    }
-    
+	document.addEventListener('DOMContentLoaded', init, false);
+
+	function init() {
+		username = document.querySelector('#username');
+		confirm_username = document.querySelector('#confirm_username');
+		checker = document.querySelector('#checker');
+		confirm_username.addEventListener('click', box_checked, false);
+	}
+
+
+
+	function checkTextField(){
+		var username_value = username.value;
+		if ( username_value !== null && username_value.trim() !== ''){
+			return false;
+		}
+		else if (username_value === null || username_value.trim() === ''){
+			return true;
+		}
+		else{
+			return true;
+		}
+	}
+
+	function box_checked(){
+		if(confirm_username.checked){
+			if(!checkTextField()){
+				username.disabled = true;
+				confirm_username.disabled = true;
+				document.getElementById("roll_die").disabled = false;
+				checker.innerHTML = 'Username is confirmed';
+			}else{
+				checker.innerHTML = 'Username hasn\'t been entered';
+			}
+		}
+	}
+
 })();
 
