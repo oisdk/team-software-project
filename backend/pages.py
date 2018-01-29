4b7736ea47da7149ea10511659c7a9002d396237
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-29 16:58:37 +0000

    Remove .py from pages again
    Docker sorts it out now, so it’s no extra effort on my part to change
    them – better to keep things as they were.

--- backend/pages.py
+++ backend/pages.py
@@ -1,5 +1,5 @@
 pages = {
-    'example.py': 'backend.example:example',
-    'request_dice_roll.py': 'backend.process_client_json:request_dice_roll',
-    'notify_turn.py': 'backend.turn:send_turn_notifications',
+    'example': 'backend.example:example',
+    'request_dice_roll': 'backend.process_client_json:request_dice_roll',
+    'notify_turn': 'backend.turn:send_turn_notifications',
 }
