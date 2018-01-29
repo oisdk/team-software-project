fd5c141bb6dca4aae5fb5364716530cb6e6e4a2c
Arash Hedayatpour <ah24@umail.ucc.ie>
2018-01-29 10:36:18 +0000

    Add code to check if visitor has visited before

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,7 +1,16 @@
 import * as sendJSON from './sendJSON';
+import * as checkUserIDCookie fromm './checkUserIDCookie';
+import * as generateUsernameForm from './generateUsernameForm';
 
 
 window.onload = () => {
+    const playerObject = checkUserIDCookie.checkUserDetails();
+    if (playerObject !== null) {
+        // Generate page for visitor to create new username
+        generateUsernameForm.generateUsernameForm();
+    } else {
+        // Generate page for visitor to create/join game
+    }
     document.getElementById('roll_die').onclick = () => {
         sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
             if (req.readyState === 4 && req.status === 200) {
