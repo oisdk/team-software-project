ea3eb0e41c80af0bbd842109b1ec1accf0115a9e
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-29 17:26:32 +0000

    Use shelf to store whose turn it is
    I want to check whether server-sent events will work for multiple
    clients and persistent storage.

--- backend/backend/turn.py
+++ backend/backend/turn.py
@@ -5,15 +5,31 @@ cgitb.enable()
 import time
 import sys
 import json
+import shelve
 
 def send_turn_notifications():
+    json_input = json.load(sys.stdin)
+    username = json_input['username']
+    create_user_entry(username)
+
     print('Content-Type: text/event-stream')
     print('Cache-Control: no cache')
     print()
     while True:
         print('event: fuck')
-        print('data: {}'.format(json.dumps({'yourTurn': True})))
+        print('data: {}'.format(json.dumps({'yourTurn': check_user_turn(username)})))
         print()
 
         sys.stdout.flush()
         time.sleep(1)
+
+def create_user_entry(username):
+    with shelve.open('turn_data') as turn_data:
+        if turn_data == {}:
+            turn_data[username] = True
+        else:
+            turn_data[username] = False
+
+def check_user_turn(username):
+    with shelve.open('turn_data') as turn_data:
+        return turn_data[username]
