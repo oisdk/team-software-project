4025e58180e13a2d1f70ef3f1d52941200811651
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-29 12:21:55 +0000

    Add blank line, flush output, and remove sleep

--- backend/backend/turn.py
+++ backend/backend/turn.py
@@ -3,11 +3,15 @@
 # cgitb.enable()
 
 import time
+import sys
 
 def send_turn_notifications():
     print('Content-Type: text/event-stream')
     print('Cache-Control: no cache')
     print()
     while True:
+        print('event: fuck')
         print('data: It’s your turn!')
-        time.sleep(5)
+        print()
+
+        sys.stdout.flush()
