6af4fc1db2cf948c0bd492f3f3929897439c121d
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-29 12:56:53 +0000

    Send json string as event data

--- backend/backend/turn.py
+++ backend/backend/turn.py
@@ -4,6 +4,7 @@ cgitb.enable()
 
 import time
 import sys
+import json
 
 def send_turn_notifications():
     print('Content-Type: text/event-stream')
@@ -11,7 +12,7 @@ def send_turn_notifications():
     print()
     while True:
         print('event: fuck')
-        print('data: It is your turn!')
+        print('data: {}'.format(json.dumps({'yourTurn': True})))
         print()
 
         sys.stdout.flush()
