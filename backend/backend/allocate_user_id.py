db05a24affc05f7beffe2ced7f5397f9153eb465
Gary Greer <thegoldenteapot@gmail.com>
2018-01-29 18:59:46 +0000

    Fixed Doctest. Added JSONEncoder function.
    UUID is not json serializable so this function casts the UUID to a string.
    Fixed issues with doctest.

--- backend/backend/allocate_user_id.py
+++ backend/backend/allocate_user_id.py
@@ -4,8 +4,17 @@ from backend.storage import Player
 import json
 import sys
 import cgitb
+from json import JSONEncoder
+from uuid import UUID
 
 cgitb.enable()
+
+"""
+Deals with no UUID serialization support in json. Casts the UUID to String.
+"""
+def JSONEncoder(uuidObject):
+    if isinstance(uuidObject, UUID):
+        return str(uuidObject)
 
 
 def request_user_id(source=sys.stdin, output=sys.stdout):
@@ -14,18 +23,16 @@ def request_user_id(source=sys.stdin, output=sys.stdout):
     >>> import io
     >>> inp = io.StringIO(json.dumps({'username': 'testuser'}))
     >>> out = io.StringIO()
-    >>> request_userID(inp,out)
+    >>> test = request_user_id(inp,out)
     >>> out.seek(0)
     0
-    >>> print(Player("testuser"))
-    testuser: []
     >>> print(out.read()) # doctest: +ELLIPSIS
     Content-Type: application/json
     <BLANKLINE>
-    {"your_username": "testuser", "your_id": testuser.user_id}
+    {"your_username": "testuser", "your_id": ...}
     """
     request = json.load(source)
     client_username = request["username"]
     player = Player(client_username)
     output.write('Content-Type: application/json\n\n')
-    json.dump({"your_username": client_username, "your_id": player.user_id}, output)
+    json.dump({"your_username": client_username, "your_id": JSONEncoder(player.user_id)}, output)
