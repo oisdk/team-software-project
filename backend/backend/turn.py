7decd08ffa3efc6caae9043c56cb2e2d34a34dc5
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-29 22:57:42 +0000

    Change representation of order to list
    Data can’t be sent to the SSE script directly, and will have to be sent
    to other functions to be stored in the database. I’ve changed the
    order’s representation to a list to match this – the SSE event can
    notify everyone of the active turn.

--- backend/backend/turn.py
+++ backend/backend/turn.py
@@ -1,35 +1,39 @@
 """Turn-related logic."""
-import cgitb
-cgitb.enable()
-
 import time
 import sys
 import json
 import shelve
 
-def send_turn_notifications():
-    json_input = json.load(sys.stdin)
-    username = json_input['username']
-    create_user_entry(username)
+import cgitb
+cgitb.enable()
 
+def send_turn_notifications():
     print('Content-Type: text/event-stream')
     print('Cache-Control: no cache')
     print()
     while True:
         print('event: fuck')
-        print('data: {}'.format(json.dumps({'yourTurn': check_user_turn(username)})))
+        print('data: {}'.format(json.dumps({'activeTurn': get_user_turn()})))
         print()
 
         sys.stdout.flush()
         time.sleep(1)
 
-def create_user_entry(username):
+def create_user_entry():
+    json_input = json.load(sys.stdin)
+    username = json_input['username']
     with shelve.open('turn_data') as turn_data:
         if turn_data == {}:
-            turn_data[username] = True
-        else:
-            turn_data[username] = False
+            turn_data['order'] = []
+        order = turn_data['order']
+        order.append(username)
+        turn_data['order'] = order
 
-def check_user_turn(username):
-    with shelve.open('turn_data') as turn_data:
-        return turn_data[username]
+def get_user_turn():
+    try:
+        with shelve.open('turn_data') as turn_data:
+            if 'order' in turn_data:
+                return turn_data['order'][0]
+            return None
+    except:
+        return None
--- backend/pages.py
+++ backend/pages.py
@@ -2,4 +2,5 @@ pages = {
     'example': 'backend.example:example',
     'request_dice_roll': 'backend.process_client_json:request_dice_roll',
     'notify_turn': 'backend.turn:send_turn_notifications',
+    'create_user_entry': 'backend.turn:create_user_entry',
 }
