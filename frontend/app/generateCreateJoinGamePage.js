5c2130cc1d398c6b83c1d72c7c40e1a2c0008ded
arhh <aulee7@icloud.com>
2018-01-29 19:00:59 +0000

    Add file reading functionality and function to update HTML body

--- frontend/app/generateCreateJoinGamePage.js
+++ frontend/app/generateCreateJoinGamePage.js
@@ -2,5 +2,12 @@
 export function generateCreateJoinGamePage() {
     // Generate a HTML page with buttons to load the "create new game" page
     // or "join existing game" page
-    document.innerHTML = 
+    const fileReader = new XMLHttpRequest();
+    fileReader.open('GET', 'create-join-game.html', true);
+    fileReader.onreadystatechange = () => updatePage(fileReader);
+}
+
+// Callback function to update HTML body with file's contents
+export function updatePage(fileReader) {
+    document.body.innerHTML = fileReader.responseText;
 }
