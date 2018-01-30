4db8e4820268fc548ebcbc76bc13ecfa0b86543b
arhh <aulee7@icloud.com>
2018-01-30 12:15:45 +0000

    Remove code from index.js calling on checkUserIDCookie This code can be re-added later since it currently fails tests

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,9 +1,7 @@
 import * as sendJSON from './sendJSON';
-// import * as checkUserIDCookie from './checkUserIDCookie';
 
 
 window.onload = () => {
-    // const playerObject = checkUserIDCookie.checkUserDetails();
     document.getElementById('roll_die').onclick = () => {
         sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
             if (req.readyState === 4 && req.status === 200) {
