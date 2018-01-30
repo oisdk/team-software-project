a38d56b40fd6af065865dbb1f6ef6f34206efd46
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-30 18:00:58 +0000

    Add comments to javascript

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -13,20 +13,30 @@ window.onload = () => {
         });
     };
 
+    // The event source receives notifications from the server
     let eventSource = new EventSource('cgi-bin/notify_turn.py');
+
+    // Once the event source has opened successfully:
     eventSource.onopen = () => {
         console.log('opened!');
+
+        // Add a listener to log errors
         eventSource.addEventListener('error', (error) => {
             console.log(`SSE error: ${error.type}`);
         });
+
+        // Add a listener to log messages
         eventSource.addEventListener('message', (message) => {
             console.log(`SSE message: ${message.data}`)
         });
+
         let turnCounter = 0;
+        // Add a listener to handle turn notifications
         eventSource.addEventListener('turn', (message) => {
             console.log(`SSE event of type turn: ${message.data}`);
             const activeTurn = JSON.parse(message.data).activeTurn;
             const username = document.querySelector('#username').value;
+            // End the turn if it’s yours and you’ve been notified 5 times
             if (activeTurn == document.querySelector('#username').value && ++turnCounter == 5) {
                 console.log('Ending turn');
                 turnCounter = 0;
@@ -39,6 +49,7 @@ window.onload = () => {
         });
     };
 
+    // Add user to the turn order when they submit their username
     document.querySelector('#usernameInput').onclick = (click) => {
         click.preventDefault();
         sendJSON.sendJSON({
