32526be2b2203b6daa18190b06317527764da384
arhh <aulee7@icloud.com>
2018-01-30 11:18:25 +0000

    Add comments to generateCreateJoinGamePage and its test file

--- frontend/app/generateCreateJoinGamePage.js
+++ frontend/app/generateCreateJoinGamePage.js
@@ -1,10 +1,13 @@
+// Import function which can read cookie values from browser cookie arrays
 import getCookieValue as getCookieValue from './checkUserIDCookie';
 
 // Callback function to update HTML body with file's contents
 export function updatePage(fileReader) {
     document.body.innerHTML = fileReader.responseText;
+    // Get the username italic field in the page's heading
     const usernameField = document.querySelector('#username');
     const username = getCookieValue(document.cookie.split('; '), 'user_name');
+    // Update the username field to include the player's username
     usernameField.innerHTML = username;
 }
 
--- frontend/app/generateCreateJoinGamePage.test.js
+++ frontend/app/generateCreateJoinGamePage.test.js
@@ -1,16 +1,22 @@
 import * as generateCreateJoinGamePage from './generateCreateJoinGamePage';
 
 describe('updatePage test', () => {
+    // Store the current state of the HTML body so it can be restored after the
+    // test
     const oldDocumentBody = document.body.innerHTML;
 
+    // Create a mock XMLHttpRequest response which contains a HTML page
     const mockFileResponse = {
         responseText: '<p>test html content<i id="username"></i></p>',
     };
 
+    // Create a user_name cookie which will be read by updatePage to customise
+    // the create/join page
     beforeAll(() => {
         document.cookie = 'user_name=testuser';
     });
 
+    // Restore the HTML body to its state before the test and delete the cookie
     afterAll(() => {
         document.body.innerHTML = oldDocumentBody;
         document.cookie = 'user_name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
@@ -27,16 +33,19 @@ describe('updatePage test', () => {
 describe('generateCreateJoinGamePage test', () => {
     const oldXMLHttpRequest = window.XMLHttpRequest;
 
+    // Create a mock XMLHttpRequest object
     const mockXHR = {
         open: jest.fn(),
         send: jest.fn(),
         setRequestHeader: jest.fn(),
     };
 
+    // Assign the global window XMLHttpRequest to point to the mockXHR object
     beforeEach(() => {
         window.XMLHttpRequest = jest.fn(() => mockXHR);
     });
 
+    // Restore the global XMLHttpRequest to the state it was before the tests
     afterEach(() => {
         window.XMLHttpRequest = oldXMLHttpRequest;
     });
