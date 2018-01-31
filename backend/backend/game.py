23e1251ddef92f66f730fd714016bc4fd0d308f7
Gary Greer <thegoldenteapot@gmail.com>
2018-01-31 19:48:17 +0000

    changes for removeng doctests. Fotrmatting errors.

--- backend/backend/game.py
+++ backend/backend/game.py
@@ -6,6 +6,13 @@ import uuid
 class Monopoly:  # pylint: disable=too-few-public-methods
     """A class representing a game of
     monopoly.
+    
+    >>> print(Monopoly(4)) # doctest: +ELLIPSIS
+    Game id:...
+    Lobby State:0/4
+    Game Ready:False
+    Players:[]
+    <BLANKLINE>
     """
     def __init__(self, gameSize):
         self._uuid = uuid.uuid1()
@@ -14,14 +21,10 @@ class Monopoly:  # pylint: disable=too-few-public-methods
         self._player_counter = 0
         self._game_ready_state = False
 
-    @property
-    def game_id(self):
-        """A unique, read-only game id"""
-        return self._uuid
 
     def __str__(self):
-        return (('Game id:%s:\n Lobby State: %d/%d\nGame Ready:%s'
-                 '\n Players:%s\n')
+        return (('Game id:%s\nLobby State:%d/%d\nGame Ready:%s'
+                 '\nPlayers:%s\n')
                 % (self._uuid, self._player_counter,
                    self._game_size, self._game_ready_state, self._players))
 
@@ -38,5 +41,6 @@ class Monopoly:  # pylint: disable=too-few-public-methods
             # Communicate readiness to client
 
     def get_game_id(self):
-        """Getter for game uuid"""
+        """Getter for game uuid
+        """
         return self._uuid
