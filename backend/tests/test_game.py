23afe7e2ddb445a94ee7a0a6920929a43e33e361
Gary Greer <thegoldenteapot@gmail.com>
2018-01-31 20:03:01 +0000

    fixed variable name for flake

--- backend/tests/test_game.py
+++ backend/tests/test_game.py
@@ -6,16 +6,16 @@ from backend.game import Monopoly
 
 class TestMonopolyGame(unittest.TestCase):
     def test_game(self):
-        testGame = Monopoly(4)
-        testGame.add_player("TestPlayer")
-        self.assertEqual(testGame._players[0], "TestPlayer")
-        self.assertEqual(testGame._player_counter, 1)
-        self.assertEqual(testGame._game_size, 4)
-        self.assertEqual(testGame._game_ready_state, False)
-        testGame.add_player("TestPlayer1")
-        testGame.add_player("TestPlayer2")
-        testGame.add_player("TestPlayer3")
-        self.assertEqual(testGame._game_ready_state, True)
+        testgame = Monopoly(4)
+        testgame.add_player("TestPlayer")
+        self.assertEqual(testgame._players[0], "TestPlayer")
+        self.assertEqual(testgame._player_counter, 1)
+        self.assertEqual(testgame._game_size, 4)
+        self.assertEqual(testgame._game_ready_state, False)
+        testgame.add_player("TestPlayer1")
+        testgame.add_player("TestPlayer2")
+        testgame.add_player("TestPlayer3")
+        self.assertEqual(testgame._game_ready_state, True)
 
 
 def load_tests(loader, tests, ignore):
