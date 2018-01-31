3a42a6977116d69ba772de9ab7c2ee969b344a03
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-31 15:41:53 +0000

    Jest test for sendJSON
    The test passes, but I’m not getting full branch coverage yet, so I’ll
    need to see what that is.

--- frontend/app/sendJSON.test.js
+++ frontend/app/sendJSON.test.js
@@ -39,3 +39,45 @@ describe('Start request test suite', () => {
         done();
     });
 });
+
+describe('sendJSON test suite', () => {
+    const oldXMLHttpRequest = window.XMLHttpRequest;
+
+    const mockXHR = {
+        open: jest.fn(),
+        send: jest.fn(),
+        setRequestHeader: jest.fn(),
+    };
+
+    beforeEach(() => {
+        window.XMLHttpRequest = jest.fn(() => mockXHR);
+    });
+
+    afterEach(() => {
+        window.XMLHttpRequest = oldXMLHttpRequest;
+    });
+
+    test('should', (done) => {
+        const succCallback = jest.fn();
+        const failureCallback = jest.fn();
+        const mockServerAddress = random.string(5);
+        const mockJSON = {test: random.string(5)};
+        sendJSON.sendJSON({
+            serverAddress: mockServerAddress,
+            successCallback: succCallback,
+            jsonObject: mockJSON,
+        });
+        expect(mockXHR.open).toHaveBeenCalledWith('POST', mockServerAddress);
+        expect(succCallback).not.toHaveBeenCalled();
+        mockXHR.readyState = 4;
+        mockXHR.state = 200;
+        mockXHR.onreadystatechange();
+        expect(succCallback).toHaveBeenCalledWith(mockXHR);
+        mockXHR.state = 404;
+        mockXHR.onreadystatechange();
+        expect(failureCallback).toHaveBeenCalledWith(mockXHR);
+        expect(mockXHR.send).toHaveBeenCalledWith(JSON.stringify(mockJSON));
+        expect(mockXHR.setRequestHeader).toHaveBeenCalledWith('Content-Type', 'application/json; charset=UTF-8');
+        done();
+    });
+});
