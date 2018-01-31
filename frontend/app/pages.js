6c531e81b4c93768fae8257ec6e5dafd0922829f
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-31 14:47:49 +0000

    Write pages module and waitingGame function
    I’ve written this to depend on the generic sendJSON function that I’m
    writing in another branch.

---
+++ frontend/app/pages.js
@@ -0,0 +1,21 @@
+import {sendJSON} from './sendJSON';
+/**
+ * This module provides functions which display a certain page to the user.
+ */
+
+/**
+ * Displays the page for a game in a waiting state.
+ *
+ * @param gameID The ID of the game to display.
+ * @param rootElement The DOM element the page will be displayed under.
+ */
+export function waitingGame({gameID, rootElement}) {
+    function displayPage(request) {
+        rootElement.innerHTML = request.responseText;
+    }
+    sendJSON({
+        serverAddress: 'cgi-bin/get_game_details.py',
+        jsonObject: {'gameID': gameID},
+        successCallback: displayPage,
+    })
+}
