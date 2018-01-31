bc6048dd4528d9e4a1556d5fe90ebaa969647a40
gabystephanov <115342151@umail.ucc.ie>
2018-01-31 11:16:10 +0000

    Add page for getting list of available games

--- backend/pages.py
+++ backend/pages.py
@@ -3,4 +3,5 @@ pages = {
     'request_dice_roll': 'backend.process_client_json:request_dice_roll',
     'receive_client_username':
     'backend.process_client_json:receive_client_username',
+    'request_games_list': 'backend.get_games_list:get_list_of_games',
 }
