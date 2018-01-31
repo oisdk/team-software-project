c34261b00f92e201e69c6739ad14c4e0a114e0dd
gabystephanov <115342151@umail.ucc.ie>
2018-01-31 12:07:36 +0000

    Function to get a list of available games
    Based on assumption for storage, this uses the database to retrieve the data for games available.

---
+++ backend/backend/get_list_of_games.py
@@ -0,0 +1,48 @@
+import pymysql as db
+
+def get_list_of_games():
+
+
+
+    """Function that returns a list of the games available
+
+       for client to join
+
+    """
+
+
+
+    listOfGames=[]
+
+    try:
+
+        connection = db.connect('cs1dev.ucc.ie', 'gss1', 'voqueixe', '2019_gss1')
+
+        cursor = connection.cursor(db.cursors.DictCursor)
+
+        cursor.execute("""SELECT name 
+
+                          FROM games
+
+                          ORDER BY name DESC""")
+
+        for row in cursor.fetchall():
+
+            listOfGames += [row['name']]        
+
+        cursor.close()  
+
+        connection.close()
+        return listOfGames
+
+    
+
+    except db.Error:
+
+        return('Unable to connect to the database.')
+
+
+
+    return listOfGames
+
+print(get_list_of_games())
