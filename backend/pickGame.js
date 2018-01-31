f30afd68753a893cb253b6fd03e0b57ec0aa8bd4
Jason Cheung <jasoncheung94@gmail.com>
2018-01-31 23:34:48 +0000

    Functions to request list of games and generate a list

---
+++ backend/pickGame.js
@@ -0,0 +1,30 @@
+export function requestGameList(){
+	
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		
+		//just an example for testing to simulate this.responseText
+		list = JSON.stringify(['game1','game2','game3','game4']);
+		pickGame(list);
+	    
+    }
+  };
+  xhttp.open("POST", "get_games.py", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send();
+}
+
+export function pickGame(list){
+	
+	// generate the nodes vs changing innerHTML of an empty div ? 
+	var ul = document.createElement('ul');
+	list = JSON.parse(list);
+	for( i in list){
+		var li = document.createElement('li');
+		var item = document.createTextNode(list[i]);
+		li.appendChild(item);
+		ul.appendChild(li);
+		document.body.appendChild(ul);
+	}
+}
