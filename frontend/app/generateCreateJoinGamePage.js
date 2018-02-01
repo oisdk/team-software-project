9d416ad4fdf144e8fae4ff97a25bf529accff86c
Arash Hedayatpour <aulee7@icloud.com>
2018-02-01 10:45:57 +0000

    Update comment to import * from checkUserIDCookie
    Getting parsing errors for the import statement when just importing getCookieValue

--- frontend/app/generateCreateJoinGamePage.js
+++ frontend/app/generateCreateJoinGamePage.js
@@ -1,12 +1,12 @@
 // Import function which can read cookie values from browser cookie arrays
-import getCookieValue as getCookieValue from './checkUserIDCookie';
+import * as checkUserIDCookie from './checkUserIDCookie';
 
 // Callback function to update HTML body with file's contents
 export function updatePage(fileReader) {
     document.body.innerHTML = fileReader.responseText;
     // Get the username italic field in the page's heading
     const usernameField = document.querySelector('#username');
-    const username = getCookieValue(document.cookie.split('; '), 'user_name');
+    const username = checkUserIDCookie.getCookieValue(document.cookie.split('; '), 'user_name');
     // Update the username field to include the player's username
     usernameField.innerHTML = username;
 }
