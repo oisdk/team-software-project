7f2032b2e00e8b5bb206cb55cb2ab3dd347e5fbf
gabystephanov <115342151@umail.ucc.ie>
2018-02-02 23:36:55 +0000

    Testing for get_list_of_games

--- backend/tests/test_get_list_of_games.py
+++ backend/tests/test_get_list_of_games.py
@@ -6,9 +6,9 @@ import get_list_of_games
 class TestGetListOfGames(unittest.TestCase):
 
     def test_game_list_returned(self):
-        games = ['game1', 'game2', 'game3']
-        list_of_games = get_list_of_games(games)
-        self.assertEqual(games == list_of_games)
+        games = ['game1', 'game2']
+        list_of_games = get_list_of_games()
+        self.assertEqual(len(games) == len(list_of_games))
 
 
 def load_tests(loader, tests, ignore):
