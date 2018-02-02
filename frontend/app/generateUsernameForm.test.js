4895b7bd13a139efeddf6abeba8f136cbc72492a
arhh <aulee7@icloud.com>
2018-02-02 18:26:03 +0000

    Change test expect() argument to pass test For some reason generateUsernameForm creates the following when run: disabled="".

--- frontend/app/generateUsernameForm.test.js
+++ frontend/app/generateUsernameForm.test.js
@@ -33,22 +33,21 @@ describe('Request sent on function call', () => {
 });
 
 describe('Generate form to the body of ', () => {
-    let page;
+    // let page;
 
     beforeAll(() => {
-        page = document.body.innerHTML;
-        document.body.innerHTML = '<form><label for="username">Enter username:</label><input type="text" id="username"><br><input type="checkbox" name="confirm_username" id="confirm_username"><span id="checker">I have confirmed my username</span><br><input type="submit" value="Submit" id="submit_username" disabled></form>';
+        // page = document.body.innerHTML;
     });
 
     afterAll(() => {
-        document.body.innerHTML = page;
+        // document.body.innerHTML = page;
     });
 
     test('if generated form is successful ', (done) => {
         const callback = jest.fn();
         usernameForm.generateUsernameForm(callback);
         expect(document.getElementById('submit_username').value).toEqual('Submit');
-        expect(document.body.innerHTML).toEqual('<form><label for="username">Enter username:</label><input type="text" id="username"><br><input type="checkbox" name="confirm_username" id="confirm_username"><span id="checker">I have confirmed my username</span><br><input type="submit" value="Submit" id="submit_username" disabled></form>');
+        expect(document.body.innerHTML).toEqual('<form><label for="username">Enter username:</label><input type="text" id="username"><br><input type="checkbox" name="confirm_username" id="confirm_username"><span id="checker">I have confirmed my username</span><br><input type="submit" value="Submit" id="submit_username" disabled=""></form>');
         expect(document.getElementById('username').type).toEqual('text');
         done();
     });
