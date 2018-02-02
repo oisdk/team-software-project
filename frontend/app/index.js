6354a4b7134632227939c0a5794031cd6b1864de
arhh <aulee7@icloud.com>
2018-02-02 18:40:33 +0000

    Remove old commented out code Since the dynamic pages are loading normally (not considering the strange errors appearing in browser), there is no need to have code written in Sprint 1 to be here anymore.

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,4 +1,3 @@
-// import * as sendJSON from './sendJSON';
 import * as checkUserIDCookie from './checkUserIDCookie';
 import * as generateUsernameForm from './generateUsernameForm';
 import * as generateCreateJoinGamePage from './generateCreateJoinGamePage';
@@ -22,14 +21,4 @@ window.onload = () => {
             }
         });
     }
-    // document.getElementById('roll_die').onclick = () => {
-    //     sendJSON.gameStartRequest('cgi-bin/request_dice_roll.py', (req) => {
-    //         if (req.readyState === 4 && req.status === 200) {
-    //             const p = document.createElement('P');
-    //             const t = document.createTextNode(req.responseText);
-    //             p.appendChild(t);
-    //             document.body.appendChild(t);
-    //         }
-    //     });
-    // };
 };
