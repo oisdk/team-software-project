1ade4a95fc386fef9cd1d000355ebea4a156c43e
gabystephanov <115342151@umail.ucc.ie>
2018-02-03 23:40:03 +0000

    Fixed page location for request_game_list

--- backend/pages.py
+++ backend/pages.py
@@ -3,7 +3,7 @@ pages = {
     'request_dice_roll': 'backend.process_client_json:request_dice_roll',
     'receive_client_username':
     'backend.process_client_json:receive_client_username',
-    'request_games_list': 'backend.get_games_list:get_list_of_games',
+    'request_games_list': 'backend.request_game_list:request_game_list',
     'allocate_user_id': 'backend.allocate_user_id:request_user_id',
     'allocate_game_id': 'backend.allocate_game_id:request_game_id',
 }
