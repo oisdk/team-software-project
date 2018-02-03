8562da064c3ce6eebd7ff46916276886d1781184
arhh <aulee7@icloud.com>
2018-02-03 15:39:09 +0000

    Add span tage to generated HTML to allow checkButton to display messages

--- frontend/app/generateUsernameForm.js
+++ frontend/app/generateUsernameForm.js
@@ -8,7 +8,7 @@ export function requestUserID(serverAddress, nameSubmitted, callback) {
 }
 
 export function generateUsernameForm(callback) {
-    document.body.innerHTML = '<form><label for="username">Enter Username</label><input type="text" id="username"><input type="submit" value="Submit" id="submit_username" disabled></form>';
+    document.body.innerHTML = '<form><label for="username">Enter Username</label><input type="text" id="username"><input type="submit" value="Submit" id="submit_username" disabled><span id="checker"></span></form>';
     document.getElementById('submit_username').addEventListener('click', (event) => { event.preventDefault(); });
     document.getElementById('submit_username').onclick = () => requestUserID('cgi-bin/allocate_user_id.py', document.getElementById('username').value, callback);
 }
