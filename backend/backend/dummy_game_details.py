ed7a121683781e8f39048f66d8d6aa02ffd40f1c
Noel Bourke <undergroundquizscene@gmail.com>
2018-02-04 19:25:29 +0000

    Add dummy implementation of get_game_details

---
+++ backend/backend/dummy_game_details.py
@@ -0,0 +1,10 @@
+"""
+Dummy implementation of python function to get details of a specific game.
+"""
+import json
+
+def get_game_details(game_id):
+    """Get the details for a game given its ID."""
+    print('Content-type: application/json; charset=UTF-8')
+    print()
+    print(json.dumps({'game': {'data': 3}}))
