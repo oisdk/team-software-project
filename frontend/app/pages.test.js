0aa42007bb2ab27b5979f3281075617527faa009
Noel Bourke <undergroundquizscene@gmail.com>
2018-02-05 13:31:01 +0000

    Dummy sendJSON to test it it’s called

--- frontend/app/pages.test.js
+++ frontend/app/pages.test.js
@@ -2,7 +2,18 @@ import waitingGame from './pages';
 import * as sJ from './sendJSON';
 
 describe('waitingGame', () => {
+    const oldSJ = sJ.sendJSON;
+
+    beforeEach(() => {
+        sJ.sendJSON = jest.fn();
+    });
+
     it('should call sendJSON', () => {
-        expect(sJ.sendJSON)toHaveBeenCalled();
+        waitingGame();
+        expect(sJ.sendJSON).toHaveBeenCalled();
+    });
+
+    afterEach(() => {
+        sJ.sendJSON = oldSJ;
     });
 });
