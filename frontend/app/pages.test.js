44172c88694cfe90f1046f5ae00bad8a8fa35aa8
Noel Bourke <undergroundquizscene@gmail.com>
2018-02-05 13:56:01 +0000

    Test waitingGame success callback

--- frontend/app/pages.test.js
+++ frontend/app/pages.test.js
@@ -1,5 +1,6 @@
 import waitingGame from './pages';
 import * as sJ from './sendJSON';
+import * as random from './random';
 
 describe('waitingGame', () => {
     const oldSJ = sJ.sendJSON;
@@ -11,6 +12,9 @@ describe('waitingGame', () => {
     it('should call sendJSON', () => {
         waitingGame();
         expect(sJ.sendJSON).toHaveBeenCalled();
+        const mockRequest = {responseText: String(random.int(0, 1000))};
+        sJ.sendJSON.mock.calls[0][0].successCallback(mockRequest);
+        expect(document.body.innerHTML).toEqual(mockRequest.responseText);
     });
 
     afterEach(() => {
