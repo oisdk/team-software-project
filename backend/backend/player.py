11d926111d7ffe02d6405249425184b357218fba
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-02-08 13:31:55 +0000

    column sizes

--- backend/backend/player.py
+++ backend/backend/player.py
@@ -32,9 +32,11 @@ class Player:
     """
     def __init__(self, username=None, uid=None):
         if username is None and uid is None:
-            raise TypeError("Player() expects one argument of either username or uid, none given")
+            raise TypeError('Player() expects one argument of either username'
+                            'or uid, none given')
         elif username is not None and uid is not None:
-            raise TypeError("Player() expects one argument of either username or uid, two given")
+            raise TypeError('Player() expects one argument of either username'
+                            'or uid, two given')
         elif username is not None:
             self.username = username
             self._new = True
@@ -47,23 +49,32 @@ class Player:
         self._conn.begin()
         if self._new:
             with self._conn.cursor() as cursor:
-                cursor.execute('INSERT INTO `players` (`username`) (%s); SELECT LAST_INSERT_ID();',
+                cursor.execute('INSERT INTO `players` (`username`) (%s);'
+                               'SELECT LAST_INSERT_ID();',
                                (self.username,))
                 self._uid = cursor.fetchone()
             self.rolls = []
         else:
             with self._conn.cursor() as cursor:
-                cursor.execute('SELECT (`username`) FROM `players` WHERE `id` = %s;', (self.uid,))
+                cursor.execute('SELECT (`username`) FROM `players`'
+                               'WHERE `id` = %s;',
+                               (self.uid,))
                 self.username = cursor.fetchone()['username']
             with self._conn.cursor() as cursor:
-                cursor.execute('SELECT (`roll1`, `roll2`) FROM  `rolls` WHERE `id` = %s ORDER BY `number`;', (self.id,))
-                self.rolls = [(result['roll1'], result['roll2']) for result in cursor.fetchall()]
+                cursor.execute('SELECT (`roll1`, `roll2`) FROM `rolls`'
+                               'WHERE `id` = %s ORDER BY `number`;',
+                               (self.id,))
+                self.rolls = [(result['roll1'], result['roll2'])
+                              for result in cursor.fetchall()]
 
     def __exit__(self):
         with self._conn.cursor() as cursor:
-            cursor.execute('UPDATE `players` SET `username` = %s WHERE `id` = %s', (self.username, self.uid))
+            cursor.execute('UPDATE `players` SET `username` = %s '
+                           'WHERE `id` = %s',
+                           (self.username, self.uid))
             cursor.execute('REPLACE INTO `rolls` VALUES (%s, %s, %s, %s)',
-                           ((self.uid, roll1, roll2, i) for i, (roll1, roll2) in enumerate(self.rolls)))
+                           ((self.uid, roll1, roll2, i)
+                            for i, (roll1, roll2) in enumerate(self.rolls)))
         self._conn.commit()
         self._conn.close()
 
