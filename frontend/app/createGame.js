170973e7543c7835b0270a8a8b4911705362143b
douglaslordan <35471251+douglaslordan@users.noreply.github.com>
2018-02-08 11:23:29 +0000

    Corrected JSON.methods

--- frontend/app/createGame.js
+++ frontend/app/createGame.js
@@ -4,15 +4,15 @@ import sendJSON from './sendJSON';
 export default function getGameID(player_count = 4) {
     sendJSON({
         serverAddress: 'cgi-bin/allocate_game_id.py',
-        jsonObject: JSON.dumps({game_size: player_count}),
+        jsonObject: JSON.stringify({game_size: player_count}),
         successCallback: (req1) => {
-            const response = JSON.load(req1.responseText);
+            const response = JSON.parse(req1.responseText);
             const gameID = response.game_id;
             sendJSON({
                 serverAddress: 'cgi-bin/waiting_game.py',
-                jsonObject: JSON.dumps({gameID}),
+                jsonObject: JSON.stringify({gameID: gameID}),
                 /* successCallback: (req2) => {
-                    // const responseSuccess = JSON.load(req2.responseText);
+                    // const responseSuccess = JSON.parse(req2.responseText);
                     // Display waiting game.
                 }, */
             });
