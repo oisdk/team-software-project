3f3d5b47e810112a69f07a2d66cb113374ebf983
arhh <aulee7@icloud.com>
2018-02-08 21:04:54 +0000

    Separate successCallback function and add parameterised sendJSON

--- frontend/app/createGame.js
+++ frontend/app/createGame.js
@@ -1,21 +1,38 @@
 // Import sendJSON functionality
-import sendJSON from './sendJSON';
+import * as sendJSON from './sendJSON';
 
-export default function getGameID(player_count = 4) {
-    sendJSON({
+// A global variable to store the function which makes the request to the server
+let JSONSender = sendJSON.sendJSON;
+
+/**
+ * Callback for when game_id successfully received.
+ *
+ * @param {XMLHttpRequest} req1 Contains the response with the game_id.
+ */
+export function successCallback(req1) {
+    const response = JSON.parse(req1.responseText);
+    const gameID = response.game_id;
+    JSONSender({
+        serverAddress: 'cgi-bin/waiting_game.py',
+        jsonObject: JSON.stringify({game_id: gameID}),
+    });
+}
+
+/**
+ * Make a request for a gameID.
+ *
+ * @param {int} player_count The number of players in the game being created.
+ * @param {Function} JSONSend The function to make the request to the server.
+ */
+export function getGameID(player_count = 4, JSONSend = sendJSON.sendJSON) {
+    // Immediately assign JSONSend parameter to global JSONSender,
+    // this is really just to make testing easier
+    JSONSender = JSONSend;
+    JSONSender({
         serverAddress: 'cgi-bin/allocate_game_id.py',
         jsonObject: JSON.stringify({game_size: player_count}),
-        successCallback: (req1) => {
-            const response = JSON.parse(req1.responseText);
-            const gameID = response.game_id;
-            sendJSON({
-                serverAddress: 'cgi-bin/waiting_game.py',
-                jsonObject: JSON.stringify({gameID: gameID}),
-                /* successCallback: (req2) => {
-                    // const responseSuccess = JSON.parse(req2.responseText);
-                    // Display waiting game.
-                }, */
-            });
-        },
+        // The below line is shorthand because the object field name and the
+        // actual function have the same name
+        successCallback,
     });
 }
--- frontend/app/createGame.test.js
+++ frontend/app/createGame.test.js
@@ -1,11 +1,46 @@
-import getGameID from './createGame';
-import sendJSON from './sendJSON';
+import * as getGameID from './createGame';
 
-jest.mock('./sendJSON');
+describe('createGame tests', () => {
+    // Create a mock for the actual sendJSON function
+    const mockSendJSON = jest.fn();
 
-describe('getGameID sendJSONCheck', () => {
-    getGameID();
+    // Backup the BOM XMLHttpRequest object to a variable since it will be
+    // over-written soon
+    const oldXHR = window.XMLHttpRequest;
 
-    expect(sendJSON).toHaveBeenCalledTimes(2);
-    expect(sendJSON).toHaveBeenCalledWith();
+    // Create a mock XMLHttpRequest with mock methods
+    const mockXHR = {
+        open: jest.fn(),
+        send: jest.fn(),
+        setRequestHeader: jest.fn(),
+    };
+
+    // Create a mock server AJAX response
+    const mockResponse = {
+        responseText: '{"game_id": 1}',
+    };
+
+    // Function run before each test() below
+    beforeEach(() => {
+        // Tell the BOM XMLHttpRequest to use the mock XMLHttpRequest
+        window.XMLHttpRequest = jest.fn(() => mockXHR);
+    });
+
+    // Function run after each test() below
+    afterEach(() => {
+        // Restore the BOM XMLHttpRequest to its original state
+        window.XMLHttpRequest = oldXHR;
+    });
+
+    test('getGameID', (done) => {
+        getGameID.getGameID(4, mockSendJSON);
+        expect(mockSendJSON).toHaveBeenCalled();
+        done();
+    });
+
+    test('successCallback', (done) => {
+        getGameID.successCallback(mockResponse);
+        expect(mockSendJSON).toHaveBeenCalled();
+        done();
+    });
 });
