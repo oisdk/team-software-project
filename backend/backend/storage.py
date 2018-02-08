3c1bad3689f34f70fda830107ebc7e8a934df796
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-02-08 11:21:06 +0000

    basic function for creating connection to the database

--- backend/backend/storage.py
+++ backend/backend/storage.py
@@ -1,57 +1,11 @@
 """Module implementing storage for the app"""
 
-import uuid
+import pymysql.cursors
 
-
-class Player:  # pylint: disable=too-few-public-methods
-    """A class representing a player of
-    monopoly.
-
-    Initialisation of an instance of this class will store
-    it in the database automatically. Similarly, any
-    mutation of the public fields will result in mutation
-    in the database.
-
-    >>> print(Player("dave"))
-    dave: []
-    """
-    def __init__(self, username):
-        self.username = username
-        self.rolls = []
-        self._uuid = uuid.uuid1()
-
-    @property
-    def user_id(self):
-        """A unique, read-only user id"""
-        return self._uuid
-
-    def __str__(self):
-        return '%s: %s' % (self.username, self.rolls)
-
-    def __eq__(self, other):
-        return self.user_id == other.user_id
-
-
-class DatabaseLookupError(LookupError):
-    """Errors for when a key is not found in the database"""
-    def __init__(self, key_desc, key_val):
-        LookupError.__init__(self)
-        self.key_desc = key_desc
-        self.key_val = key_val
-
-    def __str__(self):
-        return 'Could not find entry for %s key %s' % (self.key_desc,
-                                                       self.key_val)
-
-
-def retrieve_player(user_id):
-    """Retieves a player from the database.
-    Raises a DatabaseLookupError if not found.
-
-    >>> retrieve_player(123)
-    Traceback (most recent call last):
-        ...
-    backend.storage.DatabaseLookupError: \
-Could not find entry for user_id key 123
-    """
-    raise DatabaseLookupError("user_id", user_id)
+def make_connection():
+    return pymysql.connect(host='localhost',
+                           user='root',
+                           password='',
+                           db='db',
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
