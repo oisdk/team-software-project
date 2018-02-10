17cb39412dd87c2d2108d8564f69072a4bbf0cd6
gabystephanov <115342151@umail.ucc.ie>
2018-02-10 22:23:13 +0000

    Create start_game.py

---
+++ backend/backend/start_game.py
@@ -0,0 +1,30 @@
+"""Module providing functionality to update an existing
+   game's state.
+"""
+
+
+import json
+import sys
+import cgitb
+import backend.storage
+
+
+cgitb.enable()
+
+
+def start_game(source=sys.stdin):
+    """Updates a requested game's state to 'playing' by
+       its game id.
+    """
+    request = json.load(source)
+    game_id = request["game_id"]
+    try:
+        conn = backend.storage.make_connection()
+        conn.begin()
+        with self._conn.cursor() as cursor:
+            cursor.execute('UPDATE `games` '
+                               'SET `state` = %s'
+                               'WHERE `id` = %s;',
+                               ('playing', game_id))
+    finally:
+        conn.close()
