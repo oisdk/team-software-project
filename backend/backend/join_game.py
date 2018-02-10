9eb55506e32baa017191fc284c4bab77c4ed7bc7
Gary Greer <thegoldenteapot@gmail.com>
2018-02-10 11:51:14 +0000

    possible fix

--- backend/backend/join_game.py
+++ backend/backend/join_game.py
@@ -1,7 +1,8 @@
 """Module enables players to join a chosen game and adds the player to the turn order"""
 
 def join_game(game_id, player_id):
-    with Game(game_id) as game and Player(player_id) as player:
+    with Player(player_id) as player:
+        game = Game(game_id)
         if game.state == "waiting":
             player.turn_position = len(game.players) + 1
         else:
