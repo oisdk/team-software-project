9eac3341f164711f5b5ad66d3b007191f51489fc
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-02-11 20:40:17 +0000

    modified function to work with json input

--- backend/backend/join_game.py
+++ backend/backend/join_game.py
@@ -1,8 +1,19 @@
 """Module enables players to join a chosen game and adds the player to the turn order"""
 from backend.game import Game
 from backend.player import Player
+import json
+import sys
+import cgitb
 
-def join_game(game_id, player_id):
+cgitb.enable()
+
+def join_game(source=sys.stdin):
+    """Entry point for the client joining game on server
+    """
+    request = json.load(source)
+    player_id = request["user_id"]
+    game_id = request["game_id"]
+
     with Player(player_id) as player:
         with Game(game_id) as game:
             players = game.players
