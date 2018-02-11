20371dde29a5d3294551b3a91a613263bab709a5
Noel Bourke <undergroundquizscene@gmail.com>
2018-02-11 15:25:44 +0000

    Make changes to pass eslint

--- frontend/app/pages.js
+++ frontend/app/pages.js
@@ -1,7 +1,39 @@
-import {sendJSON} from './sendJSON';
 /**
  * This module provides functions which display a certain page to the user.
  */
+
+/**
+ * Creates the html for the waiting game.
+ *
+ * @param gameID The id of the game.
+ * @param {HTMLElement} rootElement The element to which the html is added.
+ * @param {String} playerListID The id the player list element should have.
+ * @param {String} startButtonID The id the start game button should have.
+ */
+function createHTML({
+    gameID,
+    rootElement,
+    playerListID,
+    startButtonID,
+}) {
+    while (rootElement.firstChild) {
+        rootElement.firstChild.remove();
+    }
+
+    const heading = document.createElement('h1');
+    heading.innerHTML = `You are in game ${gameID}`;
+    const playerList = document.createElement('section');
+    playerList.id = playerListID;
+    const startButton = document.createElement('button');
+    startButton.style.type = 'button';
+    startButton.innerHTML = 'Start Game';
+    startButton.id = startButtonID;
+    startButton.disabled = true;
+
+    rootElement.appendChild(heading);
+    rootElement.appendChild(playerList);
+    rootElement.appendChild(startButton);
+}
 
 /**
  * Displays the page for a game in a waiting state.
@@ -24,43 +56,16 @@ export default function waitingGame(gameID) {
     sseEventSource.addEventListener('playerJoin', (joinEvent) => {
         const playerList = JSON.parse(joinEvent.data);
         const playerListElement = document.getElementById(playerListID);
-        for (player of playerList) {
+        for (let i = 0; i < playerList.length; i += 1) {
+            const player = playerList[i];
             const playerElement = document.createElement('div');
             playerElement.innerHTML = player.username;
             playerListElement.appendChild(playerElement);
 
-            numberOfPlayers++;
+            numberOfPlayers += 1;
             if (numberOfPlayers === 4) {
                 document.getElementById(startButtonID).disabled = false;
             }
         }
     });
-}
-
-/**
- * Creates the html for the waiting game.
- *
- * @param gameID The id of the game.
- * @param {HTMLElement} rootElement The element to which the html is added.
- * @param {String} playerListID The id the player list element should have.
- * @param {String} startButtonID The id the start game button should have.
- */
-function createHTML({gameID, rootElement, playerListID, startButtonID}) {
-    while (rootElement.firstChild) {
-        rootElement.firstChild.remove();
-    }
-
-    const heading = document.createElement('h1');
-    heading.innerHTML = `You are in game ${gameID}`;
-    const playerList = document.createElement('section');
-    playerList.id = playerListId;
-    const startButton = document.createElement('button');
-    startButton.style.type = 'button';
-    startButton.innerHTML = 'Start Game';
-    startButton.id = startButtonID;
-    startButton.disabled = true;
-
-    rootElement.appendChild(heading);
-    rootElement.appendChild(playerList);
-    rootElement.appendChild(startButton);
 }
