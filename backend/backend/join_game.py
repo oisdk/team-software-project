4d27fe45f46dfa96e06d4ef23bffe9e722a4c7c1
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-02-11 18:27:33 +0000

    working join_game function. NEed to decide on else condition

--- backend/backend/join_game.py
+++ backend/backend/join_game.py
@@ -5,6 +5,9 @@ from backend.player import Player
 def join_game(game_id, player_id):
     with Player(player_id) as player:
         with Game(game_id) as game:
+            players = game.players
+            players.append(player_id)
+            game.players = players
             state = str(game.state)
             if str(game.state) == 'waiting':
                 player.turn_position = len(game.players)
