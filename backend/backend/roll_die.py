624b4923d255f1260b0a972fd7d2a4ba7818fe77
Adam O Sullivan <adamosullivan46@gmail.com>
2018-02-11 17:18:28 +0000

    Added passing go money

--- backend/backend/roll_die.py
+++ backend/backend/roll_die.py
@@ -27,12 +27,20 @@ def player_roll_dice(game_id, player_id):
     """Rolls two dice for a player, appends there rolls to the database,
        updates their position and the current game turn.
     """
+
+    NUM_SQUARES = 40
+    PASS_GO_AMAOUNT = 200
+
     with Game(game_id) as game:
         with Player(player_id) as player:
             if game.current_turn == player.turn_position:
                 rolls = roll_two_dice()
                 player.rolls.append(rolls)
                 player.board_position += sum(rolls)
+
+                if board_position >= NUM_SQUARES:
+                    player.balance += PASS_GO_AMAOUNT
+                    player.board_position -= NUM_SQUARES
 
                 if rolls[0] != rolls[1]:
                     if game.current_turn == len(game.players)-1:
