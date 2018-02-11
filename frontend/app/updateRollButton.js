b1effba34aaba8e4a558f4ffd7424e69f5b19fa9
gabystephanov <115342151@umail.ucc.ie>
2018-02-11 22:56:21 +0000

    Add functions for checking roll button

---
+++ frontend/app/updateRollButton.js
@@ -0,0 +1,7 @@
+// Enables the roll button for the player if the previous
+// roll values are equal
+export function checkRollValuesEqual(){...}
+
+// Adds event listener to the roll button for the player to
+// enable/disable the button
+export function validateRollButton(){...}
