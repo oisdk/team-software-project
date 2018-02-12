ae50906f2a38ac74d537957829c75e5310d408f1
Jason Cheung <jasoncheung94@gmail.com>
2018-02-12 18:54:00 +0000

    Changed root element to get inner html of div with id of content

--- frontend/app/pages.test.js
+++ frontend/app/pages.test.js
@@ -4,9 +4,18 @@ import * as random from './random';
 
 describe('waitingGame', () => {
     const oldSJ = sJ.sendJSON;
+    const oldDocumentBody = document.body;
+
+    beforeAll(() => {
+        document.body.innerHTML = '<div id="content"></div>';
+    });
 
     beforeEach(() => {
         sJ.sendJSON = jest.fn();
+    });
+
+    afterAll(() => {
+        document.body = oldDocumentBody;
     });
 
     it('should call sendJSON', () => {
@@ -15,7 +24,7 @@ describe('waitingGame', () => {
         expect(sJ.sendJSON).toHaveBeenCalled();
         const mockRequest = {responseText: String(random.int(0, 1000))};
         sJ.sendJSON.mock.calls[0][0].successCallback(mockRequest);
-        expect(document.body.innerHTML).toEqual(`You are in the waiting game ${mockGameId}.
+        expect(document.getElementById('content').innerHTML).toEqual(`You are in the waiting game ${mockGameId}.
             Here are the details:
             ${mockRequest.responseText}`);
     });
