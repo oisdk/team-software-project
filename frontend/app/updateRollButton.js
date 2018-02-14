2f4a5e464486ed4afa353e3470674fe2f1f7060a
gabystephanov <115342151@umail.ucc.ie>
2018-02-14 15:40:20 +0000

    Includes parsing JSON
    Assuming for now the button's ID is called 'roll'.

--- frontend/app/updateRollButton.js
+++ frontend/app/updateRollButton.js
@@ -1,13 +1,18 @@
 // Enables the roll button for the player if the previous
 // roll values are equal
-export function checkRollValuesEqual(){...}
-
+export function checkRollValuesEqual() {
+    if (xhttp.readyState === 4 && xhttp.status === 200){
+		const state = JSON.parse(xhttp.responseText);
+		const roll_die = document.querySelector('#roll');
+		roll_die.disabled = !state;
+	}
+}
 // Adds event listener to the roll button for the player to
 // enable/disable the button
 export function validateRollButton() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => checkRollValuesEqual(xhttp);
-    xhttp.open('POST', 'cgi-bin/...', true);
+    xhttp.open('POST', 'cgi-bin/compare_dice_rolls.py', true);
     xhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhttp.send();
 }
