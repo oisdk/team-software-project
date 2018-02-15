2234869ec4581f65c6b672f14100db22ad8510d3
gabystephanov <115342151@umail.ucc.ie>
2018-02-15 00:00:31 +0000

    Renamed function and fixed npm error

--- frontend/app/updateRollButton.js
+++ frontend/app/updateRollButton.js
@@ -1,15 +1,15 @@
 // Enables the roll button for the player if the previous
 // roll values are equal
-export function checkRollValuesEqual() {
+export function checkRollValuesEqual(xhttp) {
     if (xhttp.readyState === 4 && xhttp.status === 200){
 		const state = JSON.parse(xhttp.responseText);
-		const roll_die = document.querySelector('#roll');
-		roll_die.disabled = !state;
+		const rollDie = document.querySelector('#roll');
+		rollDie.disabled = !state;
 	}
 }
 // Adds event listener to the roll button for the player to
 // enable/disable the button
-export function validateRollButton() {
+export function requestCompareRolls() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => checkRollValuesEqual(xhttp);
     xhttp.open('POST', 'cgi-bin/compare_dice_rolls.py', true);
