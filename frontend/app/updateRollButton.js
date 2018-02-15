289f9778de48e84251b1f6eb9c26f2c50d9baeae
gabystephanov <115342151@umail.ucc.ie>
2018-02-15 00:22:23 +0000

    Fixed spacing

--- frontend/app/updateRollButton.js
+++ frontend/app/updateRollButton.js
@@ -1,11 +1,11 @@
 // Enables the roll button for the player if the previous
 // roll values are equal
 export function checkRollValuesEqual(xhttp) {
-    if (xhttp.readyState === 4 && xhttp.status === 200){
-		const state = JSON.parse(xhttp.responseText);
-		const rollDie = document.querySelector('#roll');
-		rollDie.disabled = !state;
-	}
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        const state = JSON.parse(xhttp.responseText);
+        const rollDie = document.querySelector('#roll');
+        rollDie.disabled = !state;
+    }
 }
 // Adds event listener to the roll button for the player to
 // enable/disable the button
