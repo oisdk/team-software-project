2d3978c01dface9762ab3b5e538a3d89f90bc816
gabystephanov <115342151@umail.ucc.ie>
2018-02-15 10:14:52 +0000

    Replace ajax with calling sendJSON to python script

--- frontend/app/updateRollButton.js
+++ frontend/app/updateRollButton.js
@@ -1,18 +1,24 @@
+// Import sendJSON functionality
+import * as sendJSON from './sendJSON';
+// Import getCookie functionality
+import * as getCookie from './checkUserIDCookie';
+
 // Enables the roll button for the player if the previous
 // roll values are equal
-export function checkRollValuesEqual(xhttp) {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
-        const state = JSON.parse(xhttp.responseText);
-        const rollDie = document.querySelector('#roll');
-        rollDie.disabled = !state;
-    }
+export function checkRollValuesEqual(req) {
+    const state = JSON.parse(req.responseText);
+    const rollDie = document.querySelector('#roll');
+    rollDie.disabled = !state;
 }
+
 // Adds event listener to the roll button for the player to
 // enable/disable the button
-export function requestCompareRolls() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => checkRollValuesEqual(xhttp);
-    xhttp.open('POST', 'cgi-bin/compare_dice_rolls.py', true);
-    xhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhttp.send();
+export function requestCompareRolls(JSONSend = sendJSON.sendJSON) {
+    const details = getCookie.checkUserDetails();
+    const id = details.user_id;
+    JSONSend({
+        serverAddress: 'cgi-bin/compare_dice_rolls.py',
+        jsonObject: {player_id: id},
+        checkRollValuesEqual,
+    });
 }
