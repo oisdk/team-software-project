f0390916ef98e378702a64a95b7673616258432d
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-02-16 17:18:35 +0000

    initial userDetails generation

---
+++ frontend/app/generateUserDetails.js
@@ -0,0 +1,43 @@
+// Imports
+import * as getCookie from './checkUserIDCookie';
+import {initialiseEventSource} from './sse';
+
+const details = getCookie.checkUserDetails();
+const id = details.user_id;
+/**
+ * Callback function to update HTML body with file's contents.
+ * @param {XMLHttpRequest} fileReader - Contains local file with HTML to display.
+ */
+export function updateUserDetails(fileReader) {
+    if (fileReader.status === 200 && fileReader.readyState === 4) {
+        document.getElementById('content-left').innerHTML = fileReader.responseText;
+    }
+}
+
+/**
+ * Function to generate game details. Makes a request to local
+ * filesystem for a HTML file to display.
+ * @param {int} gameID - id used to create eventSource.
+ */
+export function generateGameInterface(gameID) {
+    // Generate a HTML page with user interface
+    const fileReader = new XMLHttpRequest();
+    fileReader.open('GET', 'user-info.html', true);
+    fileReader.onreadystatechange = () => updateGamePage(fileReader);
+    fileReader.send();
+
+    document.getElementById('username').innerHTML = details.user_name;
+
+    // SSE Events
+    const sseEventSource = initialiseEventSource(gameID);
+    sseEventSource.addEventListener('playerTurn', (turnEvent) => {
+        const turn = JSON.parse(turnEvent.data);
+        document.getElementById('current-turn').innerHTML = `Player ${turn}`;
+    });
+    sseEventSource.addEventListener('playerBalance', (turnEvent) => {
+        const balance = JSON.parse(balanceEvent.data);
+        document.getElementById('current-turn').innerHTML = balance[id];
+    });
+
+    //TODO Properties!
+}
