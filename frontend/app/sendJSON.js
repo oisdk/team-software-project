ca39ba8c6dbe42b14631aef70ebc4516a1795997
Noel Bourke <undergroundquizscene@gmail.com>
2018-02-16 17:03:56 +0000

    Remove unused functions in sendJSON.js

--- frontend/app/sendJSON.js
+++ frontend/app/sendJSON.js
@@ -1,17 +1,3 @@
-// Generate JSON to send to server on game start.
-export function generateGameStartJSON() {
-    return JSON.stringify({type: 'gameStart'});
-}
-
-// Send a POST request to the server for the game start.
-export function gameStartRequest(serverAddress, callback) {
-    const ajaxRequest = new XMLHttpRequest();
-    ajaxRequest.open('POST', serverAddress, true);
-    ajaxRequest.onreadystatechange = () => callback(ajaxRequest);
-    ajaxRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    ajaxRequest.send(generateGameStartJSON());
-}
-
 export const noOp = (_) => {};
 
 /**
