72d39c82d4b1386f000234c6a3af4f30a518b4fa
gabystephanov <115342151@umail.ucc.ie>
2018-02-19 10:32:24 +0000

    test charge rent

---
+++ backend/tests/test_charge_rent.py
@@ -0,0 +1,14 @@
+import unittest
+import doctest
+import backend.charge_rent
+
+class TestChargeRent(unittest.TestCase):
+    pass
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(backend.charge_rent))
+    return tests
+
+
+if __name__ == '__main__':
+    unittest.main()
