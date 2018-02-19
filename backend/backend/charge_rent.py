4a02edfc2784d1c55361caa4bc70c5a776894457
gabystephanov <115342151@umail.ucc.ie>
2018-02-19 10:42:45 +0000

    Removed unnecessary 'pass'

--- backend/backend/charge_rent.py
+++ backend/backend/charge_rent.py
@@ -21,7 +21,6 @@ def charge_rent(source=sys.stdin, output=sys.stdout):
 
     with Player(player_id) as player:
         player_position = player.board_position
-        pass
         # get the property's position
         # get who owns that property
         # if the current player this turn doesn't own it
@@ -29,4 +28,4 @@ def charge_rent(source=sys.stdin, output=sys.stdout):
             # current player's balance reduces by rent amount
             # property owner's balance increases by rent amount
 
-    json.dump({"player_id": "balance"}, output)
+    json.dump({"player_id": player_position}, output)
