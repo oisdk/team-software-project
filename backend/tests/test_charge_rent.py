8392d77280f2194a26d1a6e6964384cda96e7f61
gabystephanov <115342151@umail.ucc.ie>
2018-02-19 10:49:24 +0000

    Fixed flake8 issue

--- backend/tests/test_charge_rent.py
+++ backend/tests/test_charge_rent.py
@@ -2,8 +2,10 @@ import unittest
 import doctest
 import backend.charge_rent
 
+
 class TestChargeRent(unittest.TestCase):
     pass
+
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(backend.charge_rent))
