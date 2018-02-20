4e05b8ac64283c5da765c334bde075abe00072bf
Jason Cheung <jasoncheung94@gmail.com>
2018-02-20 21:19:39 +0000

    Added layers to displayBoard from Brians code

--- frontend/app/activeGame.js
+++ frontend/app/activeGame.js
@@ -58,10 +58,19 @@ function onPlayerBalance(playerBalanceEvent) {}
  */
 function displayBoard() {
     console.log('displayBoard called');
-    document.getElementById('content').innerHTML = '<canvas id="myCanvas" width="450" height="450" style="border:1px solid #000000;">';
-    const c = document.getElementById('myCanvas');
+    // need to change id's;
+    document.getElementById('content').innerHTML = '<canvas id="gameBoard" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index : 0;"></canvas>';
+    document.getElementByID('content').append('<canvas id="player1" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index :1";></canvas>');
+    document.getElementByID('content').append('<canvas id="player2" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index :2";></canvas>');
+    document.getElementByID('content').append('<canvas id="player3" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index :3";></canvas>');
+    document.getElementByID('content').append('<canvas id="player4" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index :4";></canvas>');
+    document.getElementByID('content').append('<canvas id="game-info" height="800" width = "800" style="position: absolute; left: 0 ; top: 0 ;z-index :5;"></canvas>');
+    const c = document.getElementById('gameBoard');
     const ctx = c.getContext('2d');
     const img = new Image();
-    img.src = 'monopoly.jpeg';
-    ctx.drawImage(img, 0, 0);
+    img.onload = function (){
+        ctx.drawImage(img, 0, 0);
+    }
+    img.src = 'monopoly.jpg';
+    
 }
