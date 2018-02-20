81950626bed6596261dba74d35a056c50659b8d2
Brian Twohig <btwohig8@gmail.com>
2018-02-20 14:42:07 +0000

    made slight changes t othe asset folder structure added different canvases for each player and added a layer to display info like player turn start or rolled a double etc

--- frontend/app/loadCanvas-Board.js
+++ frontend/app/loadCanvas-Board.js
@@ -4,9 +4,15 @@ export function loadCanvas(gameID){
   //checks if a game id has been provided and then creats the canvas
   if(gameID !== null){
     const id = gameID;
-    document.getElementById('content').innerHTML = '<canvas id="gameBoard" height="600" width = "602"></canvas>';
+    document.getElementById('content').innerHTML = '<canvas id="gameBoard" height="800" width = "800" style="z-index : 0"></canvas>';
+    document.getElementByID('content').append('<canvas id="player1" height="800" width = "800" style="z-index :1"></canvas>');
+    document.getElementByID('content').append('<canvas id="player2" height="800" width = "800" style="z-index :2"></canvas>');
+    document.getElementByID('content').append('<canvas id="player3" height="800" width = "800" style="z-index :3"></canvas>');
+    document.getElementByID('content').append('<canvas id="player4" height="800" width = "800" style="z-index :4"></canvas>');
+    document.getElementByID('content').append('<canvas id="game-info" height="800" width = "800" style="z-index :5"></canvas>');
     loadBoard();
   }
+
   else{
     document.getElementByID('content').innerHTML = '<p>An error has occured with creating the game: Game ID could not be resolved<p>';
   }
@@ -19,6 +25,6 @@ function loadBoard(){
   img.onload = function(){
     ctx.drawImage(img , 0 , 0);
   }
-  img.src = 'http://weclipart.com/gimg/E934D7AB9F40FD25/monopoly-board-game-template-wallpaper-game-gallery-clipart.jpeg';
+  img.src = '/images/monopoly.jpeg';
 
 }
