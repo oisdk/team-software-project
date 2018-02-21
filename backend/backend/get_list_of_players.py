7453064d4801f4187d550cef41a7c4ebe3dc841e
Jason Cheung <jasoncheung94@gmail.com>
2018-02-21 15:21:56 +0000

    Added docstring

--- backend/backend/get_list_of_players.py
+++ backend/backend/get_list_of_players.py
@@ -12,6 +12,9 @@ cgitb.enable()
 
 
 def request_list_of_players(source=sys.stdin, output=sys.stdout):
+    """Entry point for the service of requesting the list of
+    players in a specfic game
+    """
     output.write('Content-Type: application/json\n\n')
     request = json.load(source)
     game_id = request["game_id"]
