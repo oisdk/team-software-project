c3f7075dca5b97c2f2a1d757d6a4a2f1e0120110
gabystephanov <115342151@umail.ucc.ie>
2018-02-22 22:39:58 +0000

    is_player_bankrupt module

---
+++ backend/backend/is_bankrupt.py
@@ -0,0 +1,23 @@
+"""Module for providing the functionality to
+   detirmine if a player is declared bankrupt
+"""
+
+import json
+import sys
+import cgitb
+import backend.player import Player
+import backend.game import Game
+import backend.properties import Property
+
+cgitb.enable()
+
+def is_bankrupt(source=sys.stdin, output=sys.stdout):
+    """Player is checked if they are bankrupt or not"""
+    pass
+
+
+def remove_player(player_id):
+    """Player is removed from game with their status updated
+       and appropriate changes commence (turn order, properties...)
+    """
+    pass
