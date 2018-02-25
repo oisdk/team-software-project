3a0bacbfc6f33f03103ef4fe4e76fc3c85c93183
gabystephanov <115342151@umail.ucc.ie>
2018-02-25 22:27:52 +0000

    Function is called from server-side

--- backend/backend/is_bankrupt.py
+++ backend/backend/is_bankrupt.py
@@ -2,40 +2,31 @@
    detirmine if a player is declared bankrupt
 """
 
-import json
-import sys
-import cgitb
 from backend.player import Player
 from backend.game import get_games
 from backend.properties import Property, get_properties
 
-cgitb.enable()
 
-
-def is_bankrupt(source=sys.stdin, output=sys.stdout):
+def is_bankrupt(player_id):
     """Player is checked if they are bankrupt or not"""
-
-    output.write('Content-Type: application/json\n\n')
-    request = json.load(source)
-    player_id = request['player_id']
-
-    with Player(player_id) as player:
-        if player.balance < 0:
-            player_remove(player_id)
+    player = Player(player_id)
+    if player.balance < 0:
+        player_remove(player)
 
 
-def player_remove(player_id):
+def player_remove(player):
     """Player is removed from game with their status updated
        and appropriate changes commence (turn order, properties...)
     """
+    player_id = player.uid
+
     games = get_games()
-    with Player(player_id) as player:
-        for game in games:
-            if player.username in games[game]:
-                game_id = game
-                # Removes player from the game's list of players
-                game.players.remove(player_id)
-                break
+    for game in games:
+        if player.username in games[game]:
+            game_id = game
+            # Removes player by id from the game's list of players
+            game.players.remove(player_id)
+            break
 
     # This part receives properties owned by player by their
     # position and marks each property as 'unowned'
