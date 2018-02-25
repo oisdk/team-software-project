c6d3c4ec0fa6c5c8515cdd2785f51a0ca3a0d07c
gabystephanov <115342151@umail.ucc.ie>
2018-02-25 23:00:34 +0000

    Removed 'is_bankrupt' page

--- backend/pages.py
+++ backend/pages.py
@@ -14,5 +14,4 @@ pages = {
     'join_game': 'backend.join_game:join_game',
     'increment_turn': 'backend.increment_turn:increment_turn',
     'charge_rent': 'backend.charge_rent:charge_rent',
-    'is_player_bankrupt': 'backend.is_bankrupt:is_bankrupt',
 }
