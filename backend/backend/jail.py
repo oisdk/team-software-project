537fd83f7deef9cf6ff9c351bb70e26e611929f0
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-02-27 20:16:54 +0000

    fixed go_to_jail function and added cgitb stuff

--- backend/backend/jail.py
+++ backend/backend/jail.py
@@ -1,6 +1,11 @@
 """ Jail module """
 from backend.player import Player
 from backend.game import Game
+import sys
+import cgitb
+import json
+
+cgitb.enable()
 
 
 def go_to_jail(player_id):
@@ -18,11 +23,16 @@ def leave_jail(player_id):
     with Player(player_id) as player:
         player.jail_state = 'not_in_jail'
 
-def pay_to_leave_jail(player_id):
+def pay_to_leave_jail(source=sys.stdin, output=sys.stdout):
 
     """Function that removes a player from jail for a fee
     """
+    output.write("Content-Type: text/plain\n")
+    output.write("\n")
+
+    request = json.load(source)
+    player_id = request["player_id"]
     with Player(player_id) as player:
         player.balance -= 50
         player.jail_state = 'not_in_jail'
-
+    json.dump({player_id: 'not_in_jail'}, output)
