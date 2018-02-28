ff6d72adbbe2895eaeffd7fde84fdb09f44dd1f9
arhh <aulee7@icloud.com>
2018-02-28 21:32:19 +0000

    Update variable names and spacing for flake/lint

--- backend/backend/activate_card.py
+++ backend/backend/activate_card.py
@@ -2,14 +2,17 @@
 
 from random import randint
 from backend.player import Player
-from backend.property import Property
+from backend.game import Game
+from backend.properties import Property
+from backend.properties import get_properties
 from backend.cards import get_card_details
 
-size_of_card_deck = 15
+SIZE_OF_CARD_DECK = 15
 
-def activate_card(player_id, game_id):
+
+def activate_card(player_id, game_id):  # pylint: disable=too-many-locals
     # Pick a random number to index the card deck in the database
-    card_table_id = randint(0, size_of_card_deck)
+    card_table_id = randint(0, SIZE_OF_CARD_DECK)
 
     # Dive into database to get the card details
     card_details = get_card_details(card_table_id)
@@ -18,7 +21,7 @@ def activate_card(player_id, game_id):
     card_type = card_details["operation_type"]
 
     # Send the type and description to the client
-    card_description = card_details["description"]
+    # card_description = card_details["description"]
     pass
 
     # Get value
@@ -34,7 +37,7 @@ def activate_card(player_id, game_id):
             # or simply *jump* to a specific space on the board
             if card_value < 0:
                 # The card value here indicates how many spaces to move back
-                player.board_position -= int(card_value) # CHECK TYPES
+                player.board_position -= int(card_value)  # CHECK TYPES
             else:
                 # The card value here indicates the board position to move to
                 player.board_position = int(card_value)
@@ -49,9 +52,9 @@ def activate_card(player_id, game_id):
         this_player_properties = get_properties(player_id)[player_id]
         # Go through each property
         for property_position in this_player_properties:
-            with Property(game_id, property_position) as p:
-                total_houses += p.houses
-                total_houses += (p.hotels * 4)
+            with Property(game_id, property_position) as property_:
+                total_houses += property_.houses
+                total_houses += (property_.hotels * 4)
         # The card_value here indicates how much to pay for a single house
         to_pay = total_houses * card_value
         with Player(player_id) as player:
@@ -61,13 +64,13 @@ def activate_card(player_id, game_id):
     if card_type == "get_money":
         with Player(player_id) as player:
             # The card_value here indicates how much money to add to balance
-            player.balance += int(card_value) # CHECK TYPES
+            player.balance += int(card_value)  # CHECK TYPES
 
     # If it's a "pay_bank" type of chance card
     if card_type == "pay_bank":
         with Player(player_id) as player:
             # The card_value here indicates how much to deduct from balance
-            player.balance -= int(card_value) # CHECK TYPES
+            player.balance -= int(card_value)  # CHECK TYPES
 
     if card_type == "collect_from_opponents":
         # Get a list of *opponents* in this game
@@ -81,7 +84,7 @@ def activate_card(player_id, game_id):
             with Player(opponent_id) as opponent:
                 # card_value here indicates the amount to deduct from
                 # opponent's balance
-                opponent.balance -= int(card_value) # Check types
+                opponent.balance -= int(card_value)  # Check types
 
         # Add to this player's balance the total amount deducted from the
         # opponents.
