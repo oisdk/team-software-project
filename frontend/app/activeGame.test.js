21bebebc13043ea1fcd828cf7013c72d98db4ca9
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-02-28 15:26:00 +0000

    added new button to testing

--- frontend/app/activeGame.test.js
+++ frontend/app/activeGame.test.js
@@ -28,7 +28,7 @@ describe('onPlayerTurn', () => {
     const mockPlayerTurnEvent = {data: '1'};
 
     beforeAll(() => {
-        document.body.innerHTML = '<table><tr><td id="current-turn"></td></tr></table><button id="roll-dice">Roll Dice</button><button id="end-turn">End Turn</button>';
+        document.body.innerHTML = '<table><tr><td id="current-turn"></td></tr></table><button id="roll-dice">Roll Dice</button><button id="end-turn">End Turn</button><button id="jail">Jail</button>';
     });
 
     afterAll(() => {
