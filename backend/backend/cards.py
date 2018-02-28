baf68392d629fec1030f53fa8eb6e436a09d7d1d
arhh <aulee7@icloud.com>
2018-02-28 19:30:40 +0000

    Add get_card_details() to cards.py

---
+++ backend/backend/cards.py
@@ -0,0 +1,15 @@
+""" Module to provide accessors for the "cards" database table. """
+
+def get_card_details(unique_id):
+    conn = backend.storage.make_connection()
+    try:
+        conn.begin()
+        with conn.cursor() as cursor:
+            cursor.execute('SELECT card_type, description, operation, operation_value '
+                           'FROM cards '
+                           'WHERE unique_id = %s;', (unique_id))
+            result = cursor.fetchone()
+        return result
+
+    finally:
+        conn.close()
