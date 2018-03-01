7baa9d9e6c6c0e51a74f0c222d4115f0bf60a1ec
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-03-01 18:02:49 +0000

    add call to send player to jail in pages

--- backend/pages.py
+++ backend/pages.py
@@ -16,4 +16,5 @@ pages = {
     'request_players': 'backend.get_list_of_players:request_list_of_players',
     'charge_rent': 'backend.charge_rent:charge_rent',
     'leave_jail': 'backend.jail:pay_to_leave_jail',
+    'go_to_jail': 'backend.jail:go_to_jail',
 }
