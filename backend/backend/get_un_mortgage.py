9f71aa56f7418cfef1a522d4d6abb6ea2760d962
gabystephanov <115342151@umail.ucc.ie>
2018-03-01 16:30:44 +0000

    Removed redundant code

--- backend/backend/get_un_mortgage.py
+++ backend/backend/get_un_mortgage.py
@@ -3,7 +3,7 @@
    the database.
 """
 
-from backend.properties import get_properties_by_state as get_prop
+from backend.properties import get_properties_by_state
 
 
 def get_un_mortgage(player_id, prop_state):
@@ -11,4 +11,4 @@ def get_un_mortgage(player_id, prop_state):
     properties.
     Returns {player_id: [state, [list of properties]]}
     """
-    return {"player_id": [prop_state, [get_prop(player_id, prop_state)]]}
+    return get_properties_by_state(player_id, prop_state)
