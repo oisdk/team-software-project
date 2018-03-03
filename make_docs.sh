331b7850bfedd2c0de50698db2792e7526245ba8
Noel Bourke <undergroundquizscene@gmail.com>
2018-03-03 15:49:37 +0000

    Use function for common details

--- make_docs.sh
+++ make_docs.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-pandoc backend/readme.rst \
-        -o docs/backend/readme.pdf \
-        --template docs/templates/eisvogel.latex  \
-        --standalone \
-        --listings \
-        --table-of-contents \
-        --variable=titlepage \
-        --resource-path=backend
+##
+# Calls pandoc with a set of options.
+#
+# Arguments:
+#       $1: The input filename.
+#       $2: The output filename – usually should be .pdf.
+#       $3: The resource path that will be searched for images. Uses the same
+#               format as PATH.
+##
+function call_pandoc {
+        pandoc "$1" \
+                -o "$2" \
+                --template docs/templates/eisvogel.latex  \
+                --standalone \
+                --listings \
+                --table-of-contents \
+                --variable=titlepage \
+                --resource-path="$3"
+}
 
-pandoc frontend/readme.md \
-        -o docs/frontend/readme.pdf \
-        --template docs/templates/eisvogel.latex  \
-        --standalone \
-        --listings \
-        --table-of-contents \
-        --variable=titlepage \
-        --resource-path=frontend
+call_pandoc README.md docs/README.pdf .
+call_pandoc CONTRIBUTING.md docs/CONTRIBUTING.pdf .
+call_pandoc frontend/README.md docs/frontend/README.pdf frontend
+call_pandoc backend/README.rst docs/backend/README.pdf backend
