302f9982618ae0d5aeb032411a1e8ecec975ceb5
gabystephanov <115342151@umail.ucc.ie>
2018-03-03 16:37:25 +0000

    Now includes the player_id

--- fake_properties.sql
+++ fake_properties.sql
@@ -1,13 +1,13 @@
 /* Set the player_id for all the statements */
 
 UPDATE properties,
-SET player_id = ..., mortgaged = 'mortgaged',
+SET player_id = 1, mortgaged = 'mortgaged',
 WHERE property_position = 8;
 
 UPDATE properties,
-SET player_id = ..., mortgaged = 'unmortgaged',
+SET player_id = 1, mortgaged = 'unmortgaged',
 WHERE property_position = 6;
 
 UPDATE properties,
-SET player_id = ..., mortgaged = 'unmortgaged',
+SET player_id = 1, mortgaged = 'unmortgaged',
 WHERE property_position = 21;
