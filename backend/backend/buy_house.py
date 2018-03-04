ffe2bcbc5f9ef1b21ef0a06284e0dfd8af01c08e
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-03-04 17:19:13 +0000

    function to add house to given property name

--- backend/backend/buy_house.py
+++ backend/backend/buy_house.py
@@ -11,28 +11,31 @@ from backend.game import get_games
 cgitb.enable()
 
 
-def add_house(source=sys.stdin, output=sys.stdout):
+def add_house(source, output=sys.stdout):
     """Adds a house to a property.
     """
     request = json.load(source)
     player_id = request["player_id"]
     property_name = request["property_name"]
-    game_id = None
     property_position = get_position_by_name(player_id, property_name)
+    position = next(iter(property_position.values()))
     games = get_games()
+    houses = None
 
     with Player(player_id) as player:
         for game in games:
             if player.username in games[game]:
                 game_id = game
                 break
-        with Property(property_position, game_id) as prop:
+        with Property(position, game_id) as prop:
             if prop.houses < 4:
                 prop.houses += 1
+                houses = prop.houses
             else:
                 prop.houses = 0
                 prop.hotels = 1
+                houses = 5
             player.balance -= prop.house_price
 
     output.write('Content-Type: application/json\n\n')
-    json.dumps({"house_purchased": player_id}, output)
+    json.dump({"house_number": houses, "property_position": position}, output)
