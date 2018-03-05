ece6596f8becca5f952a05e5c9b2c6623d2cd8be
TheGoldenTeapot <TheGoldenTeapot@Gmail.com>
2018-03-05 21:11:42 +0000

    fixed broken test

--- frontend/app/generateUserDetails.test.js
+++ frontend/app/generateUserDetails.test.js
@@ -9,7 +9,7 @@ describe('rollDice endTurn successCallback tests', () => {
 
     // Create buttons to test
     beforeAll(() => {
-        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button>';
+        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button><button id="properties-house">Buy House</button>';
     });
 
     // Restore the HTML body
@@ -27,120 +27,43 @@ describe('rollDice endTurn successCallback tests', () => {
     test('endTurn', (done) => {
         generateUserDetails.endTurn(mockSendJSON);
         expect(mockSendJSON).toHaveBeenCalled();
-        expect(document.getElementById('roll-dice').hasAttribute('disabled'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-
-    test('successCallback', (done) => {
-        generateUserDetails.successCallback(mockResponse);
-        jest.spyOn(global.console, 'log');
-        expect(document.getElementById('roll-dice').hasAttribute('disabled=""'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-});
-
-describe('disable button tests', () => {
-    // Create a mock for the actual sendJSON function
-    const oldDocumentBody = document.body;
-
-
-    // Create buttons to test
-    beforeAll(() => {
-        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button>';
-    });
-
-    // Restore the HTML body
-    afterAll(() => {
-        document.body.innerHTML = oldDocumentBody;
-    });
-
-
-    test('disableGameInterface', (done) => {
-        generateUserDetails.disableGameInterface();
-        expect(document.getElementById('roll-dice').hasAttribute('disabled'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-
-    test('enableGameInterface', (done) => {
-        generateUserDetails.enableGameInterface();
-        expect(document.getElementById('roll-dice').hasAttribute('disabled=""'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-
-    test('enableEndTurn', (done) => {
-        generateUserDetails.enableEndTurn();
-        expect(document.getElementById('roll-dice').hasAttribute('disabled'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled=""'));
-        done();
-    });
-});
-
-describe('rollDice endTurn successCallback tests', () => {
-    // Create a mock for the actual sendJSON function
-    const mockSendJSON = jest.fn();
-    const mockResponse = {responseText: '{"your_rolls": "(1,2)"}'};
-    const oldDocumentBody = document.body;
-
-
-    // Create buttons to test
-    beforeAll(() => {
-        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button>';
-    });
-
-    // Restore the HTML body
-    afterAll(() => {
-        document.body.innerHTML = oldDocumentBody;
-    });
-
-
-    test('rollDice', (done) => {
-        generateUserDetails.rollDice(mockSendJSON);
-        expect(mockSendJSON).toHaveBeenCalled();
-        done();
-    });
-
-    test('endTurn', (done) => {
-        generateUserDetails.endTurn(mockSendJSON);
-        expect(mockSendJSON).toHaveBeenCalled();
-        expect(document.getElementById('roll-dice').hasAttribute('disabled'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-
-    test('successCallback', (done) => {
-        generateUserDetails.successCallback(mockResponse);
-        jest.spyOn(global.console, 'log');
-        expect(document.getElementById('roll-dice').hasAttribute('disabled=""'));
-        expect(document.getElementById('end-turn').hasAttribute('disabled'));
-        done();
-    });
-});
-
-describe('disable button tests', () => {
-    // Create a mock for the actual sendJSON function
-    const oldDocumentBody = document.body;
-
-
-    // Create buttons to test
-    beforeAll(() => {
-        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button>';
-    });
-
-    // Restore the HTML body
-    afterAll(() => {
-        document.body.innerHTML = oldDocumentBody;
-    });
-
-
-    test('disableGameInterface', (done) => {
-        generateUserDetails.disableGameInterface();
         expect(document.getElementById('roll-dice').hasAttribute('disabled'));
         expect(document.getElementById('end-turn').hasAttribute('disabled'));
         expect(document.getElementById('jail').hasAttribute('disabled'));
+        expect(document.getElementById('properties-house').hasAttribute('disabled'));
+        done();
+    });
+
+    test('successCallback', (done) => {
+        generateUserDetails.successCallback(mockResponse);
+        jest.spyOn(global.console, 'log');
+        expect(document.getElementById('roll-dice').hasAttribute('disabled=""'));
+        expect(document.getElementById('end-turn').hasAttribute('disabled'));
+        done();
+    });
+});
+
+describe('disable button tests', () => {
+    // Create a mock for the actual sendJSON function
+    const oldDocumentBody = document.body;
+
+
+    // Create buttons to test
+    beforeAll(() => {
+        document.body.innerHTML = '<button id="roll-dice"></button><button id="end-turn"></button><button id="jail">Jail</button><button id="properties-house">Buy House</button>';
+    });
+
+    // Restore the HTML body
+    afterAll(() => {
+        document.body.innerHTML = oldDocumentBody;
+    });
+
+
+    test('disableGameInterface', (done) => {
+        generateUserDetails.disableGameInterface();
+        expect(document.getElementById('roll-dice').hasAttribute('disabled'));
+        expect(document.getElementById('end-turn').hasAttribute('disabled'));
+        expect(document.getElementById('properties-house').hasAttribute('disabled'));
         done();
     });
 
@@ -158,7 +81,6 @@ describe('disable button tests', () => {
         done();
     });
 });
-
 
 /*
 describe('onPlayerMove', () => {
