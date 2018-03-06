d5c5dc5ee54aa15608f0b3d03216731e17cb30a7
Noel Bourke <undergroundquizscene@gmail.com>
2018-03-06 12:14:24 +0000

    Check players in game object

--- backend/tests/database_testing.py
+++ backend/tests/database_testing.py
@@ -23,9 +23,12 @@ def create_test_game(usernames):
 
 def main():
     """Creates a test game with some players."""
+    from backend.game import Game
     players = ['Kate', 'Beth', 'Alice', 'Naomi']
     game_id = create_test_game(players)
     print('Created game with id {}'.format(game_id))
+    game = Game(game_id)
+    print('Game object has player ids: {}'.format(game.players))
 
 if __name__ == '__main__':
     main()
