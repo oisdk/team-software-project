a3c465f5161e01b3aeb1fffca5ae63244713523f
Noel Bourke <undergroundquizscene@gmail.com>
2018-03-07 00:26:27 +0000

    Revert "Reduce number of players required for testing"
    This reverts commit 13483ce20ebf06f49f50995be2fcd68d2d246fa1.

--- frontend/app/waitingGame.js
+++ frontend/app/waitingGame.js
@@ -81,7 +81,7 @@ export function waitingGame(gameID) {
             }
             playersInGame.push(player);
             // Only enable the "start game" button when 4 players have joined
-            if (numberOfPlayers === 2) {
+            if (numberOfPlayers === 4) {
                 document.getElementById(startButtonID).disabled = false;
                 // Add an event listener to the "start game" button which makes
                 // a request to start-game.py to update the status of this game
