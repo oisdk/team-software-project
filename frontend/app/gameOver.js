a5ef21d06d2f7b7eb18c3f778a2d1d3ccefab3c0
Noel Bourke <undergroundquizscene@gmail.com>
2018-03-09 00:15:11 +0000

    Display Game Over page to users

--- frontend/app/gameOver.js
+++ frontend/app/gameOver.js
@@ -1,3 +1,4 @@
+import {checkUserDetails} from './checkUserIDCookie';
 /**
  * Handles user interaction once a game has finished.
  * @module
@@ -10,6 +11,35 @@
  */
 export function gameOver(gameEndEvent) {
     const eventData = JSON.parse(gameEndEvent);
-    console.log('Game over!');
-    console.log(`The winner was ${eventData.winner.name}!`);
+
+    const rootElement = document.getElementById('content');
+    while (rootElement.firstChild) {
+        rootElement.firstChild.remove();
+    }
+    rootElement.appendChild(createGameOverHTML(eventData.winner));
+}
+
+/**
+ * Creates the html for the Game Over page.
+ *
+ * @param {Object} winner The object containing the winner’s info, which has a "name" field and an
+ *     "id" field.
+ * @return {HTMLElement} The root element of the newly-created DOM subtree.
+ */
+function createGameOverHTML(winner) {
+    const container = document.createElement('section');
+
+    const header = document.createElement('h1');
+    header.textContent = 'Game over!';
+    container.appendChild(header);
+
+    const paragraph = document.createElement('p');
+    if (winner.id === parseInt(checkUserDetails.user_id, 10)) {
+        paragraph.textContent = 'You win!';
+    } else {
+        paragraph.textContent = `You lose – the winner is ${winner.name}!`;
+    }
+    container.appendChild(paragraph);
+
+    return container;
 }
